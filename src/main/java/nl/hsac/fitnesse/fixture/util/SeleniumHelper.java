4b5403af154617f90b480dce23014b3f9ceafe6d
Fried Hoeben <bitbucket@hsac.nl>
2013-06-21 17:25:09 +0200

    First steps to support web application tests using selenium

---
+++ src/main/java/nl/hsac/fitnesse/fixture/util/SeleniumHelper.java
@@ -0,0 +1,126 @@
+package nl.hsac.fitnesse.fixture.util;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Helper to work with Selenium.
+ */
+public class SeleniumHelper {
+
+    private static WebDriver WEB_DRIVER = null;
+    private static boolean SHUTDOWN_HOOK_ENABLED = false;
+    /** Timeout in milliseconds to wait for element to appear */
+    public static int IMPLICIT_WAIT = 15000;
+
+    public static WebDriver getWebDriver() {
+        if(WEB_DRIVER == null) {
+            if(!SHUTDOWN_HOOK_ENABLED) {
+                Runtime.getRuntime().addShutdownHook(new Thread() {
+                    public void run() {
+                        close();
+                    }
+                });
+                SHUTDOWN_HOOK_ENABLED = true;
+            }
+			/*
+			 * Vervang dit eventueel door een browser naar keuze.
+			 */
+            WEB_DRIVER = new FirefoxDriver();
+        }
+
+        setImplicitWait(IMPLICIT_WAIT);
+        return WEB_DRIVER;
+    }
+
+    private static void setImplicitWait(int implicitWait) {
+//        WEB_DRIVER.manage().timeouts().implicitlyWait(implicitWait, TimeUnit.MILLISECONDS);
+    }
+
+    public static void close() {
+        if(WEB_DRIVER != null) {
+            WEB_DRIVER.quit();
+            WEB_DRIVER = null;
+        }
+    }
+
+    public WebElement getElement(String place) {
+        WebElement element = null;
+        setImplicitlyWait(5);
+        String xpathPlace = place.replace("\"", "&quot;")
+                                 .replace("'", "&apos;").trim();
+        if (element == null) {
+            WebElement label = findElement(By.xpath("//label[text()='" + xpathPlace + "']"));
+            if (label != null) {
+                String forAttr = label.getAttribute("for");
+                element = findElement(By.id(forAttr));
+            }
+        }
+        if (element == null) {
+            element = findElement(By.xpath("//*[@aria-label='" + xpathPlace + "']"));
+        }
+        if (element == null) {
+            element = findElement(By.name(place));
+        }
+        if (element == null) {
+            element = findElement(By.id(place));
+        }
+        setImplicitlyWait(IMPLICIT_WAIT);
+        return element;
+    }
+
+    public void setImplicitlyWait(int implicitWait) {
+        setImplicitWait(implicitWait);
+    }
+
+    public WebElement findElement(By by) {
+        WebElement element = null;
+        List<WebElement> elements = getWebDriver().findElements(by);
+        if (elements.size() == 1) {
+            element = elements.get(0);
+        } else if (elements.size() > 1) {
+            elements = elementsWithId(elements);
+            if (elements.size() == 1) {
+                element = elements.get(0);
+            } else {
+                throw new RuntimeException("Multiple elements with id found for: " + by
+                                            + ":\n" + elementsAsString(elements));
+            }
+        }
+        return element;
+    }
+
+    private List<WebElement> elementsWithId(List<WebElement> elements) {
+        List<WebElement> result = new ArrayList<WebElement>(1);
+        for (WebElement e : elements) {
+            String attr = e.getAttribute("id");
+            if (attr != null && !attr.isEmpty()) {
+                result.add(e);
+            }
+        }
+        return result;
+    }
+
+    private String elementsAsString(Collection<WebElement> elements) {
+        StringBuilder b = new StringBuilder();
+        b.append("[");
+        boolean first = true;
+        for (WebElement e : elements) {
+            if (first) {
+                first = false;
+            } else {
+                b.append(", ");
+            }
+            b.append(e.getAttribute("id"));
+        }
+        b.append("]");
+        return b.toString();
+    }
+
+}
---
+++ src/main/java/nl/hsac/fitnesse/fixture/web/BrowserTest.java
@@ -0,0 +1,89 @@
+package nl.hsac.fitnesse.fixture.web;
+
+import nl.hsac.fitnesse.fixture.util.SeleniumHelper;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BrowserTest {
+    private static final Pattern PATTERN = Pattern.compile("<a href=\"(.*?)\">(.*?)</a>", Pattern.CASE_INSENSITIVE);
+
+    private List<List<String>> commands = new ArrayList<List<String>>();
+    private SeleniumHelper seleniumHelper = new SeleniumHelper();
+
+    public boolean open(String htmlLink) {
+        String url = urlFromLink(htmlLink);
+        commands.add(Arrays.asList("open", url));
+        SeleniumHelper.getWebDriver().navigate().to(url);
+        return true;
+    }
+
+    public boolean enterAs(String value, String place) {
+        return enterFor(value, place);
+    }
+
+    public boolean enterFor(String value, String place) {
+        commands.add(Arrays.asList("enter", place, value));
+        getElement(place).sendKeys(value);
+        return true;
+    }
+
+    public boolean selectAs(String value, String place) {
+        return selectFor(value, place);
+    }
+
+    public boolean selectFor(String value, String place) {
+        commands.add(Arrays.asList("select", place, value));
+        return true;
+    }
+
+    public boolean click(String place) {
+        commands.add(Arrays.asList("click", place));
+        getElement(place).click();
+        return true;
+    }
+
+    public boolean valueOfIs(String place, String expectedValue) {
+        return valueForIs(place, expectedValue);
+    }
+
+    public boolean valueForIs(String place, String expectedValue) {
+        commands.add(Arrays.asList("valueForIs", place, expectedValue));
+        return true;
+    }
+
+    public String valueOf(String place) {
+        return valueFor(place);
+    }
+
+    public String valueFor(String place) {
+        commands.add(Arrays.asList("valueFor", place));
+        return "Bob.";
+    }
+
+    public void clear(String place) {
+        commands.add(Arrays.asList("clear", place));
+        getElement(place).clear();
+    }
+
+    private WebElement getElement(String place) {
+        return seleniumHelper.getElement(place);
+    }
+
+    public List<List<String>> commands() {
+        return commands;
+    }
+
+    private String urlFromLink(String htmlLink) {
+        String result = null;
+        Matcher matcher = PATTERN.matcher(htmlLink);
+        if (matcher.matches()) {
+            result = matcher.group(1);
+        }
+        return result;
+    }
+}
