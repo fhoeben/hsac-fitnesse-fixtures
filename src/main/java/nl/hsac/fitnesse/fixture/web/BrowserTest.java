5ef9de15d2c0b496700fd41f6cbc9fda5cda47cc
Fried Hoeben <bitbucket@hsac.nl>
2013-06-23 22:53:00 +0200

    Support for, and tests that use: - getting current page title - clicking links - selecting select options - retrieving current text for input - retrieving current selected option of select

--- src/main/java/nl/hsac/fitnesse/fixture/web/BrowserTest.java
+++ src/main/java/nl/hsac/fitnesse/fixture/web/BrowserTest.java
@@ -1,25 +1,25 @@
 package nl.hsac.fitnesse.fixture.web;
 
 import nl.hsac.fitnesse.fixture.util.SeleniumHelper;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class BrowserTest extends SlimFixture {
     private static final Pattern PATTERN = Pattern.compile("<a href=\"(.*?)\">(.*?)</a>", Pattern.CASE_INSENSITIVE);
 
-    private List<List<String>> commands = new ArrayList<List<String>>();
     private SeleniumHelper seleniumHelper = getEnvironment().getSeleniumHelper();
 
     public boolean open(String htmlLink) {
         String url = urlFromLink(htmlLink);
-        commands.add(Arrays.asList("open", url));
         seleniumHelper.navigate().to(url);
         return true;
+    }
+
+    public String pageTitle() {
+        return seleniumHelper.getPageTitle();
     }
 
     public boolean enterAs(String value, String place) {
@@ -28,7 +28,6 @@ public class BrowserTest extends SlimFixture {
 
     public boolean enterFor(String value, String place) {
         boolean result = false;
-        commands.add(Arrays.asList("enter", place, value));
         WebElement element = getElement(place);
         if (element != null) {
             element.sendKeys(value);
@@ -42,13 +41,34 @@ public class BrowserTest extends SlimFixture {
     }
 
     public boolean selectFor(String value, String place) {
-        commands.add(Arrays.asList("select", place, value));
-        return true;
+        // choose option for select, if possible
+        boolean result = clickSelectOption(place, value);
+        if (!result) {
+            // try to click the first element with right value
+            result = click(value);
+        }
+        return result;
+    }
+
+    private boolean clickSelectOption(String selectPlace, String optionValue) {
+        boolean result = false;
+        WebElement element = getElement(selectPlace);
+        if (element != null) {
+            if (isSelect(element)) {
+                String id = element.getAttribute("id");
+                By optionWithText = seleniumHelper.byXpath("//select[@id='%s']//option[text()='%s']", id, optionValue);
+                WebElement option = seleniumHelper.findElement(element, true, optionWithText);
+                if (option != null) {
+                    option.click();
+                    result = true;
+                }
+            }
+        }
+        return result;
     }
 
     public boolean click(String place) {
         boolean result = false;
-        commands.add(Arrays.asList("click", place));
         WebElement element = getElement(place);
         if (element != null) {
             element.click();
@@ -57,27 +77,34 @@ public class BrowserTest extends SlimFixture {
         return result;
     }
 
-    public boolean valueOfIs(String place, String expectedValue) {
-        return valueForIs(place, expectedValue);
-    }
-
-    public boolean valueForIs(String place, String expectedValue) {
-        commands.add(Arrays.asList("valueForIs", place, expectedValue));
-        return true;
-    }
-
     public String valueOf(String place) {
         return valueFor(place);
     }
 
     public String valueFor(String place) {
-        commands.add(Arrays.asList("valueFor", place));
-        return "Bob.";
+        String result = null;
+        WebElement element = getElement(place);
+        if (element != null) {
+            if (isSelect(element)) {
+                String id = element.getAttribute("id");
+                By selectedOption = seleniumHelper.byXpath("//select[@id='%s']//option[@selected]", id);
+                WebElement option = seleniumHelper.findElement(element, true, selectedOption);
+                if (option != null) {
+                    result = option.getText();
+                }
+            } else {
+                result = element.getAttribute("value");
+            }
+        }
+        return result;
+    }
+
+    private boolean isSelect(WebElement element) {
+        return "select".equalsIgnoreCase(element.getTagName());
     }
 
     public boolean clear(String place) {
         boolean result = false;
-        commands.add(Arrays.asList("clear", place));
         WebElement element = getElement(place);
         if (element != null) {
             element.clear();
@@ -88,10 +115,6 @@ public class BrowserTest extends SlimFixture {
 
     private WebElement getElement(String place) {
         return seleniumHelper.getElement(place);
-    }
-
-    public List<List<String>> commands() {
-        return commands;
     }
 
     private String urlFromLink(String htmlLink) {
