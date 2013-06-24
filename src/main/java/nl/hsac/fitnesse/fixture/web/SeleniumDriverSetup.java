4e64c66a1b0d2329730618e52452cb2175c02857
Fried Hoeben <bitbucket@hsac.nl>
2013-06-24 23:30:10 +0200

    Support usage of remote web driver

--- src/main/java/nl/hsac/fitnesse/fixture/web/SeleniumDriverSetup.java
+++ src/main/java/nl/hsac/fitnesse/fixture/web/SeleniumDriverSetup.java
@@ -2,7 +2,13 @@ package nl.hsac.fitnesse.fixture.web;
 
 import nl.hsac.fitnesse.fixture.Environment;
 import nl.hsac.fitnesse.fixture.util.SeleniumHelper;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Script fixture to set up webdriver to be used by Selenium tests.
@@ -30,11 +36,59 @@ public class SeleniumDriverSetup {
         Class<?> driverClass = Class.forName(driverClassName);
         Object driver = driverClass.newInstance();
         if (driver instanceof WebDriver) {
-            SeleniumHelper helper = getHelper();
-            helper.setWebDriver((WebDriver) driver);
+            setDriver((WebDriver) driver);
             result = true;
         }
         return result;
+    }
+
+    /**
+     * Connects SeleniumHelper to a remote web driver, without specifying browser version.
+     * @param browser name of browser to connect to.
+     * @param url url to connect to browser.
+     * @return true.
+     * @throws MalformedURLException if supplied url can not be transformed to URL.
+     */
+    public boolean connectToDriverForAt(String browser, String url)
+            throws MalformedURLException {
+        return connectToDriverForVersionOnAt(browser, "", Platform.ANY.name(), url);
+    }
+
+    /**
+     * Connects SeleniumHelper to a remote web driver, without specifying browser version.
+     * @param browser name of browser to connect to.
+     * @param platformName platform browser must run on.
+     * @param url url to connect to browser.
+     * @return true.
+     * @throws MalformedURLException if supplied url can not be transformed to URL.
+     */
+    public boolean connectToDriverForOnAt(String browser, String platformName, String url)
+            throws MalformedURLException {
+        return connectToDriverForVersionOnAt(browser, "", platformName, url);
+    }
+
+    /**
+     * Connects SeleniumHelper to a remote web driver.
+     * @param browser name of browser to connect to.
+     * @param version version of browser.
+     * @param platformName platform browser must run on.
+     * @param url url to connect to browser.
+     * @return true.
+     * @throws MalformedURLException if supplied url can not be transformed to URL.
+     */
+    public boolean connectToDriverForVersionOnAt(String browser, String version, String platformName, String url)
+            throws MalformedURLException {
+        Platform platform = Platform.valueOf(platformName);
+        URL remoteUrl = new URL(url);
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities(browser, version, platform);
+        RemoteWebDriver remoteWebDriver = new RemoteWebDriver(remoteUrl, desiredCapabilities);
+        setDriver(remoteWebDriver);
+        return true;
+    }
+
+    private void setDriver(WebDriver webDriver) {
+        SeleniumHelper helper = getHelper();
+        helper.setWebDriver(webDriver);
     }
 
     /**
