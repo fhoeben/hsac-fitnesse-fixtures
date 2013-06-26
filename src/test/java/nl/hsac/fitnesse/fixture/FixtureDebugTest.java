6c16baab9b320c942aaa2acf2b472049c238fdec
Fried Hoeben <bitbucket@hsac.nl>
2013-06-26 12:47:03 +0200

    Unit test to do run/debug fixtures

---
+++ src/test/java/nl/hsac/fitnesse/fixture/FixtureDebugTest.java
@@ -0,0 +1,23 @@
+package nl.hsac.fitnesse.fixture;
+
+import fitnesse.junit.JUnitHelper;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+/**
+ * Test class to allow fixture code to be debugged.
+ */
+public class FixtureDebugTest {
+    private JUnitHelper helper = UnitTestHelper.createFitnesseHelper();
+
+    @Test
+    public void testWithFitnesse() throws Exception {
+        String suite = "MyTests.SlimTests.PrismaTest1";
+        // allow suite to execute to be overriden via system property
+        String propSuite = System.getProperty("fitnesseSuiteToRun");
+        if (!StringUtils.isEmpty(propSuite)) {
+            suite = propSuite;
+        }
+        helper.assertSuitePasses(suite);
+    }
+}
--- src/test/java/nl/hsac/fitnesse/fixture/UnitTestHelper.java
+++ src/test/java/nl/hsac/fitnesse/fixture/UnitTestHelper.java
@@ -1,5 +1,10 @@
 package nl.hsac.fitnesse.fixture;
 
+import fitnesse.junit.JUnitHelper;
+import fitnesse.junit.JUnitXMLTestListener;
+import fitnesse.responders.run.ResultsListener;
+import java.io.IOException;
+import java.net.ServerSocket;
 import nl.hsac.fitnesse.fixture.util.FileUtil;
 import nl.hsac.fitnesse.fixture.util.XmlHttpResponse;
 
@@ -14,5 +19,39 @@ public class UnitTestHelper {
     public static void fillResponse(XmlHttpResponse response, String filename) {
         response.setResponse(FileUtil.loadFile(filename));
         response.setNamespaceContext(NS_CONTEXT);
+    }
+
+    /**
+     * Creates helper to run Fitnesse inside junit execution
+     * @return JUnitHelper.
+     */
+    public static JUnitHelper createFitnesseHelper() {
+        // the paths supplied are relative to the working directory set in pom.xml (i.e. wiki)
+        ResultsListener resultsListener = new JUnitXMLTestListener("../target/failsafe-reports");
+        JUnitHelper jUnitHelper = new JUnitHelper(".", "../target/fitnesse-results", resultsListener);
+        // FIT needs a local free port
+        int localPort = getRandomFreePort();
+        jUnitHelper.setPort(localPort);
+        return jUnitHelper;
+    }
+    
+    private static int getRandomFreePort() {
+        int localPort = 80;
+        ServerSocket socket = null;
+        try {
+            socket = new ServerSocket(0);
+            localPort = socket.getLocalPort();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    // just ignore
+                }
+            }
+        }
+        return localPort;
     }
 }
