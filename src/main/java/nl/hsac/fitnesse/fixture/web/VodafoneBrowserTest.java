8f124d805b861e93f9ac8bbed36caae73e9d6944
Fried Hoeben <bitbucket@hsac.nl>
2013-10-11 15:32:46 +0200

    Add test for styled pages

---
+++ src/main/java/nl/hsac/fitnesse/fixture/web/VodafoneBrowserTest.java
@@ -0,0 +1,81 @@
+package nl.hsac.fitnesse.fixture.web;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+/**
+ * Test fixture to run Selenium tests against Vodafone sites.
+ */
+public class VodafoneBrowserTest extends BrowserTest {
+    @Override
+    public boolean click(String place) {
+        boolean result = false;
+        boolean retry = true;
+        for (int i = 0;
+             !result && retry && i < secondsBeforeTimeout();
+             i++) {
+            try {
+                result = clickFirstButton(place);
+                if (!result) {
+                    result = super.click(place);
+                }
+            } catch (WebDriverException e) {
+                String msg = e.getMessage();
+                if (!msg.contains("Other element would receive the click")) {
+                    retry = false;
+                } else {
+                    waitSeconds(1);
+                }
+            }
+        }
+        return result;
+    }
+
+    private boolean clickFirstButton(String place) {
+        boolean result = false;
+        By byButtonText = getSeleniumHelper().byXpath("//button[text() = '%s']", place);
+        List<WebElement> elements = getSeleniumHelper().driver().findElements(byButtonText);
+        if (elements != null) {
+            for (WebElement element : elements) {
+                if (element.isDisplayed() && element.isEnabled()) {
+                    element.click();
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    public boolean clickR(String place) {
+        boolean result = false;
+        By byButtonText = getSeleniumHelper().byXpath("//label[contains(text(), '%s')]", place);
+        List<WebElement> elements = getSeleniumHelper().driver().findElements(byButtonText);
+        if (elements != null) {
+            for (WebElement element : elements) {
+                String id = element.getAttribute("id");
+                String name = element.getAttribute("name");
+                if (element.isDisplayed() && element.isEnabled()) {
+                    element.click();
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    public boolean waitSeconds(int i) {
+        boolean result;
+        try {
+            Thread.sleep(1 * 1000);
+            result = true;
+        } catch (InterruptedException e) {
+            result = false;
+        }
+        return result;
+    }
+}
