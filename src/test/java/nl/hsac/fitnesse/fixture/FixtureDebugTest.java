a71fb406543240e91f30bbe5c98a60097901e7be
Fried Hoeben <bitbucket@hsac.nl>
2013-10-11 15:33:05 +0200

    Debug styled signup

--- src/test/java/nl/hsac/fitnesse/fixture/FixtureDebugTest.java
+++ src/test/java/nl/hsac/fitnesse/fixture/FixtureDebugTest.java
@@ -12,7 +12,7 @@ public class FixtureDebugTest {
 
     @Test
     public void testWithFitnesse() throws Exception {
-        String suite = "VodafoneThuisTests.SignupTests.ScenarioTest";
+        String suite = "VodafoneThuisTests.SignupTests.ChunkStyledSimpleSignup";
         // allow suite to execute to be overriden via system property
         String propSuite = System.getProperty("fitnesseSuiteToRun");
         if (!StringUtils.isEmpty(propSuite)) {
