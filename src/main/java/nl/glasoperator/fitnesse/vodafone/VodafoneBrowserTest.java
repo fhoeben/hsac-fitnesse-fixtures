fdd863d92118ba9e591fdd29125dcedb0b0415ad
Fried Hoeben <bitbucket@hsac.nl>
2013-10-12 22:42:29 +0200

    Allow checking of presence of error style. And allow tests on absence of error style/text on other labels

--- src/main/java/nl/glasoperator/fitnesse/vodafone/VodafoneBrowserTest.java
+++ src/main/java/nl/glasoperator/fitnesse/vodafone/VodafoneBrowserTest.java
@@ -4,6 +4,8 @@ import nl.hsac.fitnesse.fixture.web.BrowserTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -42,6 +44,58 @@ public class VodafoneBrowserTest extends BrowserTest {
         WebElement element = getSeleniumHelper().findElement(errorXPath);
         if (element != null) {
             result = element.getText();
+        }
+        return result;
+    }
+
+    public boolean errorStyleOn(String label) {
+        boolean result = false;
+        By controlGroupXPath = getSeleniumHelper().byXpath("//div[label[text() = '%s']]", label);
+        WebElement element = getSeleniumHelper().findElement(controlGroupXPath);
+        if (element != null) {
+            String classAttr = element.getAttribute("class");
+            if (classAttr != null) {
+                String[] classes = classAttr.split(" ");
+                result = Arrays.asList(classes).contains("error");
+            }
+        }
+        return result;
+    }
+
+    public String errorsOnOthersThan(String label) {
+        String result = "";
+        By otherErrorXPath = getSeleniumHelper()
+                .byXpath("//label[text() != '%s']/following-sibling::div/p[@class='help-block' and normalize-space(text()) != '']",
+                        label);
+        List<WebElement> elements = getSeleniumHelper().driver().findElements(otherErrorXPath);
+        if (elements != null) {
+            List<String> errors = new ArrayList<String>(elements.size());
+            for (WebElement element : elements) {
+                String errorText = element.getText();
+                errors.add(errorText);
+            }
+            if (!errors.isEmpty()) {
+                result = errors.toString();
+            }
+        }
+        return result;
+    }
+
+    public String errorStyleOnOthersThan(String label) {
+        String result = "";
+        By otherErrorXPath = getSeleniumHelper()
+                .byXpath("//div[contains(@class, 'error') and label[text() != '%s']]/label",
+                        label);
+        List<WebElement> elements = getSeleniumHelper().driver().findElements(otherErrorXPath);
+        if (elements != null) {
+            List<String> labels = new ArrayList<String>(elements.size());
+            for (WebElement element : elements) {
+                String labelText = element.getText();
+                labels.add(labelText);
+            }
+            if (!labels.isEmpty()) {
+                result = labels.toString();
+            }
         }
         return result;
     }
