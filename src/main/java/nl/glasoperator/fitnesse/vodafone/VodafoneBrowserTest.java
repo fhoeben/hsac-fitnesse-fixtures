bdd9a955bf4014959d86580a93ff7c82ce85381a
Fried Hoeben <bitbucket@hsac.nl>
2013-10-12 12:58:51 +0200

    Move handling of non-displayed, non-enabled, or elements below another element to the base class

--- src/main/java/nl/glasoperator/fitnesse/vodafone/VodafoneBrowserTest.java
+++ src/main/java/nl/glasoperator/fitnesse/vodafone/VodafoneBrowserTest.java
@@ -2,7 +2,6 @@ package nl.glasoperator.fitnesse.vodafone;
 
 import nl.hsac.fitnesse.fixture.web.BrowserTest;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
@@ -12,25 +11,10 @@ import java.util.List;
  */
 public class VodafoneBrowserTest extends BrowserTest {
     @Override
-    public boolean click(String place) {
-        boolean result = false;
-        boolean retry = true;
-        for (int i = 0;
-             !result && retry && i < secondsBeforeTimeout();
-             i++) {
-            try {
-                result = clickFirstButton(place);
-                if (!result) {
-                    result = super.click(place);
-                }
-            } catch (WebDriverException e) {
-                String msg = e.getMessage();
-                if (!msg.contains("Other element would receive the click")) {
-                    retry = false;
-                } else {
-                    waitSeconds(1);
-                }
-            }
+    public boolean clickImpl(String place) {
+        boolean result = clickFirstButton(place);
+        if (!result) {
+            result = super.clickImpl(place);
         }
         return result;
     }
@@ -41,23 +25,11 @@ public class VodafoneBrowserTest extends BrowserTest {
         List<WebElement> elements = getSeleniumHelper().driver().findElements(byButtonText);
         if (elements != null) {
             for (WebElement element : elements) {
-                if (element.isDisplayed() && element.isEnabled()) {
-                    element.click();
+                if (clickElement(element)) {
                     result = true;
                     break;
                 }
             }
-        }
-        return result;
-    }
-
-    public boolean waitSeconds(int i) {
-        boolean result;
-        try {
-            Thread.sleep(1 * 1000);
-            result = true;
-        } catch (InterruptedException e) {
-            result = false;
         }
         return result;
     }
