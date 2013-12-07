e632b463f9b57bb88750253b356f7ad4aff63cdc
Fried Hoeben <bitbucket@hsac.nl>
2013-12-07 21:10:01 +0100

    Allow a configurable wait time after each scroll.

--- src/main/java/nl/hsac/fitnesse/fixture/slim/web/BrowserTest.java
+++ src/main/java/nl/hsac/fitnesse/fixture/slim/web/BrowserTest.java
@@ -16,6 +16,7 @@ public class BrowserTest extends SlimFixture {
 
     private SeleniumHelper seleniumHelper = getEnvironment().getSeleniumHelper();
     private int secondsBeforeTimeout = 10;
+    private int waitAfterScroll = 0;
     private String screenshotBase = FILES_DIR + "/screenshots/";
     private String screenshotHeight = "200";
 
@@ -273,6 +274,10 @@ public class BrowserTest extends SlimFixture {
         return getSeleniumHelper().getElement(place);
     }
 
+    public void waitMilliSecondAfterScroll(int msToWait) {
+        waitAfterScroll = msToWait;
+    }
+
     /**
      * Scrolls browser window so top of place becomes visible.
      * @param place element to scroll to.
@@ -290,6 +295,9 @@ public class BrowserTest extends SlimFixture {
      */
     protected void scrollTo(WebElement element) {
         getSeleniumHelper().executeJavascript("arguments[0].scrollIntoView(true);", element);
+        if (waitAfterScroll > 0) {
+            waitMilliSeconds(waitAfterScroll);
+        }
     }
 
     /**
