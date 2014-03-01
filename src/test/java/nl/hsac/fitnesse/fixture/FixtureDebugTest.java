f372b821ac4c5ad2dfe4cf5febb069867c92356f
Fried Hoeben <bitbucket@hsac.nl>
2014-03-01 20:29:36 +0100

    Upgrade to latest Fitnesse

--- src/test/java/nl/hsac/fitnesse/fixture/FixtureDebugTest.java
+++ src/test/java/nl/hsac/fitnesse/fixture/FixtureDebugTest.java
@@ -12,7 +12,7 @@ public class FixtureDebugTest {
 
     @Test
     public void testWithFitnesse() throws Exception {
-        String suite = "VodafoneThuisTests.SelfcareTests.LoginTests.FirstTimeSignUp.ValidFirstTimeSignUp";
+        String suite = "MyTests";
         // allow suite to execute to be overriden via system property
         String propSuite = System.getProperty("fitnesseSuiteToRun");
         if (!StringUtils.isEmpty(propSuite)) {
