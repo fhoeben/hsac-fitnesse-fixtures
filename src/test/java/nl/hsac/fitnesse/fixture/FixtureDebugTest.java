29a59c8e6de5d450f433e328104478ab0a2b161d
Fried Hoeben <bitbucket@hsac.nl>
2014-03-02 12:50:36 +0100

    Create class to run tests from jUnit in src environment, so dependent packages can just call that in their jUnit tests

--- src/test/java/nl/hsac/fitnesse/fixture/FixtureDebugTest.java
+++ src/test/java/nl/hsac/fitnesse/fixture/FixtureDebugTest.java
@@ -1,23 +1,14 @@
 package nl.hsac.fitnesse.fixture;
 
-import fitnesse.junit.JUnitHelper;
-import org.apache.commons.lang3.StringUtils;
+import nl.hsac.fitnesse.junit.FitnesseFromJUnitRunner;
 import org.junit.Test;
 
 /**
  * Test class to allow fixture code to be debugged.
  */
 public class FixtureDebugTest {
-    private JUnitHelper helper = UnitTestHelper.createFitnesseHelper();
-
     @Test
     public void testWithFitnesse() throws Exception {
-        String suite = "MyTests";
-        // allow suite to execute to be overriden via system property
-        String propSuite = System.getProperty("fitnesseSuiteToRun");
-        if (!StringUtils.isEmpty(propSuite)) {
-            suite = propSuite;
-        }
-        helper.assertSuitePasses(suite);
+        new FitnesseFromJUnitRunner().assertSuitePasses("MyTests");
     }
 }
--- src/test/java/nl/hsac/fitnesse/fixture/UnitTestHelper.java
+++ src/test/java/nl/hsac/fitnesse/fixture/UnitTestHelper.java
@@ -1,8 +1,5 @@
 package nl.hsac.fitnesse.fixture;
 
-import fitnesse.components.PluginsClassLoader;
-import fitnesse.junit.JUnitHelper;
-import fitnesse.junit.JUnitXMLTestListener;
 import nl.hsac.fitnesse.fixture.util.FileUtil;
 import nl.hsac.fitnesse.fixture.util.XmlHttpResponse;
 
@@ -17,21 +14,5 @@ public class UnitTestHelper {
     public static void fillResponse(XmlHttpResponse response, String filename) {
         response.setResponse(FileUtil.loadFile(filename));
         response.setNamespaceContext(NS_CONTEXT);
-    }
-
-    /**
-     * Creates helper to run Fitnesse inside junit execution
-     * @return JUnitHelper.
-     */
-    public static JUnitHelper createFitnesseHelper() {
-        try {
-            new PluginsClassLoader().addPluginsToClassLoader();
-        } catch (Exception e) {
-            throw new RuntimeException("Unable to adds plugins to classpath", e);
-        }
-        // the paths supplied are relative to the working directory set in pom.xml (i.e. wiki)
-        JUnitXMLTestListener resultsListener = new JUnitXMLTestListener("../target/failsafe-reports");
-        JUnitHelper jUnitHelper = new JUnitHelper(".", "../target/fitnesse-results", resultsListener);
-        return jUnitHelper;
     }
 }
