72792e889af3390de298f2cf22bc328c4a5cad69
Fried Hoeben <bitbucket@hsac.nl>
2014-03-18 14:46:11 +0100

    Add JavaDoc and move feature to override suite to execute back to test (seams a better place for it)

--- src/test/java/nl/hsac/fitnesse/fixture/FixtureDebugTest.java
+++ src/test/java/nl/hsac/fitnesse/fixture/FixtureDebugTest.java
@@ -1,14 +1,27 @@
 package nl.hsac.fitnesse.fixture;
 
 import nl.hsac.fitnesse.junit.FitnesseFromJUnitRunner;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
 /**
  * Test class to allow fixture code to be debugged.
  */
 public class FixtureDebugTest {
+    private final static String suiteOverrideVariableName = "fitnesseSuiteToRun";
+
     @Test
     public void testWithFitnesse() throws Exception {
-        new FitnesseFromJUnitRunner().assertSuitePasses("MyTests");
+        String suiteName = "MyTests";
+
+        if (!StringUtils.isEmpty(suiteOverrideVariableName)) {
+            // allow suite to execute to be overriden via system property (e.g. for use by build server)
+            String propSuite = System.getProperty(suiteOverrideVariableName);
+            if (!StringUtils.isEmpty(propSuite)) {
+                suiteName = propSuite;
+            }
+        }
+
+        new FitnesseFromJUnitRunner().assertSuitePasses(suiteName);
     }
 }
