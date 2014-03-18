1ca0bd8c53d6aef44231381f96ea8916cb2c1508
Fried Hoeben <bitbucket@hsac.nl>
2014-03-18 14:46:11 +0100

    Add JavaDoc and move feature to override suite to execute back to test (seams a better place for it)

--- src/main/java/nl/hsac/fitnesse/junit/FitnesseFromJUnitRunner.java
+++ src/main/java/nl/hsac/fitnesse/junit/FitnesseFromJUnitRunner.java
@@ -3,15 +3,21 @@ package nl.hsac.fitnesse.junit;
 import fitnesse.components.PluginsClassLoader;
 import fitnesse.junit.JUnitHelper;
 import fitnesse.junit.JUnitXMLTestListener;
-import org.apache.commons.lang3.StringUtils;
 
+/**
+ * Helper to run Fitnesse tests from JUnit tests.
+ */
 public class FitnesseFromJUnitRunner {
     private boolean loadPlugins = true;
     private String fitnesseRoot = ".";
     private String xmlOutputPath = "../target/failsafe-reports";
     private String htmlOutputPath = "../target/fitnesse-results";
-    private String suiteOverrideVariableName = "fitnesseSuiteToRun";
 
+    /**
+     * Runs a suite of Fitnesse tests. One JUnit test will be reported for each page executed.
+     * @param suiteName name of suite to run (maybe overridden by a system property
+     * @throws Exception
+     */
     public void assertSuitePasses(String suiteName) throws Exception {
         if (loadPlugins) {
             loadPlugins();
@@ -19,17 +25,12 @@ public class FitnesseFromJUnitRunner {
         JUnitXMLTestListener resultsListener = new JUnitXMLTestListener(xmlOutputPath);
         JUnitHelper jUnitHelper = new JUnitHelper(fitnesseRoot, htmlOutputPath, resultsListener);
 
-        if (!StringUtils.isEmpty(suiteOverrideVariableName)) {
-            // allow suite to execute to be overriden via system property
-            String propSuite = System.getProperty(suiteOverrideVariableName);
-            if (!StringUtils.isEmpty(propSuite)) {
-                suiteName = propSuite;
-            }
-        }
-
         jUnitHelper.assertSuitePasses(suiteName);
     }
 
+    /**
+     * Adds Fitnesse plugins to classpath.
+     */
     protected void loadPlugins() {
         try {
             new PluginsClassLoader().addPluginsToClassLoader();
@@ -38,43 +39,61 @@ public class FitnesseFromJUnitRunner {
         }
     }
 
+    /**
+     * @return whether Fitnesse's plugins will be added to classpath of test run.
+     */
     public boolean isLoadPlugins() {
         return loadPlugins;
     }
 
+    /**
+     * @param loadPlugins whether Fitnesse's plugins will be added to classpath of test run.
+     */
     public void setLoadPlugins(boolean loadPlugins) {
         this.loadPlugins = loadPlugins;
     }
 
+    /**
+     * @return (relative) location of Fitnesse's wiki pages to current directory of JVM.
+     */
     public String getFitnesseRoot() {
         return fitnesseRoot;
     }
 
+    /**
+     * @param fitnesseRoot (relative) location of Fitnesse's wiki pages to current directory of JVM.
+     */
     public void setFitnesseRoot(String fitnesseRoot) {
         this.fitnesseRoot = fitnesseRoot;
     }
 
+    /**
+     * @return (relative) location where XML files will be written describing results of Fitnesse tests.
+     */
     public String getXmlOutputPath() {
         return xmlOutputPath;
     }
 
+    /**
+     * @param xmlOutputPath (relative) location where XML files will be written describing results of Fitnesse tests.
+     */
     public void setXmlOutputPath(String xmlOutputPath) {
         this.xmlOutputPath = xmlOutputPath;
     }
 
+    /**
+     * @return (relative) location where HTML files will be written describing results of Fitnesse tests (similar to
+     *         output of test execution from Wiki).
+     */
     public String getHtmlOutputPath() {
         return htmlOutputPath;
     }
 
+    /**
+     * @param htmlOutputPath (relative) location where HTML files will be written describing results of Fitnesse tests
+     *                       (similar to output of test execution from Wiki).
+     */
     public void setHtmlOutputPath(String htmlOutputPath) {
         this.htmlOutputPath = htmlOutputPath;
-    }
-
-    public String getSuiteOverrideVariableName() {
-        return suiteOverrideVariableName;
-    }
-
-    public void setSuiteOverrideVariableName(String suiteOverrideVariableName) {
-        this.suiteOverrideVariableName = suiteOverrideVariableName;
     }
 }
