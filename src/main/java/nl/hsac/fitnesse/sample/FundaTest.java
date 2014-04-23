23e9f4721d0658f9961f29107dfde1956de8e68f
Fried Hoeben <bitbucket@hsac.nl>
2014-04-23 23:31:37 +0200

    Showcase usage of a custom subclass of BrowserTest

---
+++ src/main/java/nl/hsac/fitnesse/sample/FundaTest.java
@@ -0,0 +1,22 @@
+package nl.hsac.fitnesse.sample;
+
+import nl.hsac.fitnesse.fixture.slim.web.BrowserTest;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Fixture class customized to test funda.nl.
+ */
+public class FundaTest extends BrowserTest {
+    private static final Pattern COUNT_PATTERN = Pattern.compile("\\((\\d+)\\)");
+    public int photoCount() {
+        String countText = textByXPath("//span[@class='hits']");
+        Matcher m = COUNT_PATTERN.matcher(countText);
+        if (!m.matches()) {
+            throw new RuntimeException("Unable to determine photo count from: " + countText);
+        }
+        String counterStr = m.group(1);
+        return Integer.parseInt(counterStr);
+    }
+}
