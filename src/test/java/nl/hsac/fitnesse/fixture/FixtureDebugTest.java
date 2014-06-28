0db35d286ac0022c497d14ec0d9744266c2e061d
Fried Hoeben <github@hsac.nl>
2014-06-28 21:16:58 +0200

    Revert "Revert "Update to latest Fitnesse""
    This reverts commit df2cf318e8fe8475b5222371443de187a532ae43.

--- src/test/java/nl/hsac/fitnesse/fixture/FixtureDebugTest.java
+++ src/test/java/nl/hsac/fitnesse/fixture/FixtureDebugTest.java
@@ -1,25 +1,13 @@
 package nl.hsac.fitnesse.fixture;
 
-import nl.hsac.fitnesse.junit.FitnesseFromJUnitRunner;
-import org.apache.commons.lang3.StringUtils;
-import org.junit.Test;
+import fitnesse.junit.FitNesseSuite;
+import nl.hsac.fitnesse.junit.HsacFitNesseRunner;
+import org.junit.runner.RunWith;
 
 /**
  * Test class to allow fixture code to be debugged.
  */
+@RunWith(HsacFitNesseRunner.class)
+@FitNesseSuite.Name("HsacExamples.SlimTests.HttpTests")
 public class FixtureDebugTest {
-    private final static String suiteOverrideVariableName = "fitnesseSuiteToRun";
-
-    @Test
-    public void testWithFitnesse() throws Exception {
-        String suiteName = "HsacExamples.SlimTests.HttpTests";
-
-        // allow suite to execute to be overriden via system property (e.g. for use by build server)
-        String propSuite = System.getProperty(suiteOverrideVariableName);
-        if (!StringUtils.isEmpty(propSuite)) {
-            suiteName = propSuite;
-        }
-
-        new FitnesseFromJUnitRunner().assertSuitePasses(suiteName);
-    }
 }
