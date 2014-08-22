f7946e154efbd91c9a6869a2cc3f73fdc7483049
Fried Hoeben <github@hsac.nl>
2014-08-22 12:25:23 +0200

    Fix imports

--- src/main/java/nl/hsac/fitnesse/fixture/util/SeleniumHelper.java
+++ src/main/java/nl/hsac/fitnesse/fixture/util/SeleniumHelper.java
@@ -1,12 +1,6 @@
 package nl.hsac.fitnesse.fixture.util;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.SearchContext;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.remote.Augmenter;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
