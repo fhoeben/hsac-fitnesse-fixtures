ef7ac7c2731a1e960a8876ff171eeaa721af4ead
Fried Hoeben <github@hsac.nl>
2014-08-24 19:53:13 +0200

    Create an overview page containing the video of the last run

--- src/main/java/nl/hsac/fitnesse/junit/HsacFitNesseRunner.java
+++ src/main/java/nl/hsac/fitnesse/junit/HsacFitNesseRunner.java
@@ -3,12 +3,18 @@ package nl.hsac.fitnesse.junit;
 import fitnesse.ContextConfigurator;
 import fitnesse.FitNesseContext;
 import fitnesse.components.PluginsClassLoader;
+import fitnesse.wiki.WikiPage;
 import nl.hsac.fitnesse.fixture.Environment;
+import nl.hsac.fitnesse.fixture.slim.web.SauceLabsSeleniumSetup;
+import nl.hsac.fitnesse.fixture.util.FileUtil;
 import nl.hsac.fitnesse.junit.patchFor486.FitNesseRunner;
 import org.apache.commons.lang3.StringUtils;
+import org.junit.runner.notification.RunNotifier;
 import org.junit.runners.model.InitializationError;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.util.List;
 
 /**
  * JUnit Runner to run a FitNesse suite or page as JUnit test.
@@ -21,9 +27,11 @@ import java.io.File;
  */
 public class HsacFitNesseRunner extends FitNesseRunner {
     private final static String suiteOverrideVariableName = "fitnesseSuiteToRun";
+    private Class<?> suiteClass;
 
     public HsacFitNesseRunner(Class<?> suiteClass) throws InitializationError {
         super(suiteClass);
+        this.suiteClass = suiteClass;
         try {
             // we include images in output so build server will have single
             // directory containing both HTML results and the images created by the tests
@@ -68,5 +76,24 @@ public class HsacFitNesseRunner extends FitNesseRunner {
         new PluginsClassLoader(getFitNesseDir(suiteClass)).addPluginsToClassLoader();
 
         return super.createContext(suiteClass);
+    }
+
+    @Override
+    protected void runPages(List<WikiPage> pages, RunNotifier notifier) {
+            super.runPages(pages, notifier);
+        try {
+            String outputDir = getOutputDir(suiteClass);
+            String suiteName = getSuiteName(suiteClass);
+            String filename = suiteName + ".html";
+            String path = new File(outputDir, filename).getAbsolutePath();
+            String overviewHtml = FileUtil.streamToString(new FileInputStream(path), path);
+            if (overviewHtml != null) {
+                String videoEmbed = new SauceLabsSeleniumSetup().videoOfRun();
+                String newOverview = overviewHtml.replace("<table", videoEmbed + "<table");
+                FileUtil.writeFile(new File(outputDir, "index.html").getAbsolutePath(), newOverview);
+            }
+        } catch (Exception e) {
+        }
+
     }
 }
