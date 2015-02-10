b587a4ba6aebaf02f0913125f07b30277c5db6d0
Fried Hoeben <github@hsac.nl>
2015-02-10 21:34:10 +0100

    Move exception message handling to base class

--- src/main/java/nl/hsac/fitnesse/fixture/slim/StopTestException.java
+++ src/main/java/nl/hsac/fitnesse/fixture/slim/StopTestException.java
@@ -5,7 +5,7 @@ package nl.hsac.fitnesse.fixture.slim;
  * Especially useful for scripts/scenraios where there is no point to continue
  * if a certain problem occurs.
  */
-public class StopTestException extends RuntimeException {
+public class StopTestException extends SlimFixtureException {
 
     /**
      * Creates new.
@@ -17,11 +17,30 @@ public class StopTestException extends RuntimeException {
 
     /**
      * Creates new.
+     * @param stackTraceInWiki whether wiki should include the stack trace of this exception, or just the message
+     * @param message message for exception.
+     */
+    public StopTestException(boolean stackTraceInWiki, String message) {
+        super(stackTraceInWiki, message);
+    }
+
+    /**
+     * Creates new.
      * @param message message for exception.
      * @param cause underlying exception.
      */
     public StopTestException(String message, Throwable cause) {
         super(message, cause);
+    }
+
+    /**
+     * Creates new.
+     * @param stackTraceInWiki whether wiki should include the stack trace of this exception, or just the message
+     * @param message message for exception.
+     * @param cause underlying exception.
+     */
+    public StopTestException(boolean stackTraceInWiki, String message, Throwable cause) {
+        super(stackTraceInWiki, message, cause);
     }
 
     /**
