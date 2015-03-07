a1054ff722d11c808cc240a2d12bbc28ea289b4f
Fried Hoeben <github@hsac.nl>
2015-03-07 16:22:05 +0100

    Upgrade most scripts to latest version from 'real' protractor

--- src/main/java/nl/hsac/fitnesse/fixture/util/NgClientSideScripts.java
+++ src/main/java/nl/hsac/fitnesse/fixture/util/NgClientSideScripts.java
@@ -2,7 +2,8 @@ package nl.hsac.fitnesse.fixture.util;
 
 /**
  * Javascript scripts to perform against AngularJs sites when using Selenium
- * (copied from @link https://github.com/bbaia/protractor-net/blob/master/src/Protractor/ClientSideScripts.cs).
+ * (copied from @link https://github.com/bbaia/protractor-net/blob/master/src/Protractor/ClientSideScripts.cs
+ * and updated using @link https://github.com/angular/protractor/blob/master/lib/clientsidescripts.js).
  */
 public class NgClientSideScripts {
     /**
@@ -13,28 +14,62 @@ public class NgClientSideScripts {
      * arguments[1] {function} callback
      */
     public final static String WaitForAngular =
-            "var el = document.querySelector(arguments[0]);\n" +
-            "angular.element(el).injector().get('$browser').notifyWhenNoOutstandingRequests(callback);";
+            "var rootSelector = arguments[0];\n" +
+                    "var el = document.querySelector(rootSelector);\n" +
+                    "\n" +
+                    "  try {\n" +
+                    "    if (!window.angular) {\n" +
+                    "      throw new Error('angular could not be found on the window');\n" +
+                    "    }\n" +
+                    "    if (angular.getTestability) {\n" +
+                    "      angular.getTestability(el).whenStable(callback);\n" +
+                    "    } else {\n" +
+                    "      if (!angular.element(el).injector()) {\n" +
+                    "        throw new Error('root element (' + rootSelector + ') has no injector.' +\n" +
+                    "           ' this may mean it is not inside ng-app.');\n" +
+                    "      }\n" +
+                    "      angular.element(el).injector().get('$browser').\n" +
+                    "          notifyWhenNoOutstandingRequests(callback);\n" +
+                    "    }\n" +
+                    "  } catch (err) {\n" +
+                    "    callback(err.message);\n" +
+                    "  }";
 
     /**
-     * Tests (asynchronously) whether the angular global variable is present on a page.
-     * Retries (with 1s interval) in case the page is just loading slowly.
+     * Tests whether the angular global variable is present on a page. Retries
+     * in case the page is just loading slowly.
      *
-     * arguments[0] {int} number of times to try.
-     * arguments[1] {function} callback
+     * Asynchronous.
+     *
+     * @param {number} attempts Number of times to retry.
+     * @param {function} asyncCallback callback
      */
     public final static String TestForAngular =
+            "var asyncCallback = arguments[1];\n" +
             "var attempts = arguments[0];\n" +
-            "var check = function(n) {\n" +
-            "    if (window.angular && window.angular.resumeBootstrap) {\n" +
-            "        callback(true);\n" +
-            "    } else if (n < 1) {\n" +
-            "        callback(false);\n" +
-            "    } else {\n" +
-            "        window.setTimeout(function() {check(n - 1)}, 1000);\n" +
-            "    }\n" +
-            "};\n" +
-            "check(attempts);";
+            "  var callback = function(args) {\n" +
+                    "    setTimeout(function() {\n" +
+                    "      asyncCallback(args);\n" +
+                    "    }, 0);\n" +
+                    "  };\n" +
+                    "  var check = function(n) {\n" +
+                    "    try {\n" +
+                    "      if (window.angular && window.angular.resumeBootstrap) {\n" +
+                    "        callback([true, null]);\n" +
+                    "      } else if (n < 1) {\n" +
+                    "        if (window.angular) {\n" +
+                    "          callback([false, 'angular never provided resumeBootstrap']);\n" +
+                    "        } else {\n" +
+                    "          callback([false, 'retries looking for angular exceeded']);\n" +
+                    "        }\n" +
+                    "      } else {\n" +
+                    "        window.setTimeout(function() {check(n - 1);}, 1000);\n" +
+                    "      }\n" +
+                    "    } catch (e) {\n" +
+                    "      callback([false, e]);\n" +
+                    "    }\n" +
+                    "  };\n" +
+                    "  check(attempts);";
 
     /**
      * Continue to bootstrap Angular.
@@ -50,8 +85,13 @@ public class NgClientSideScripts {
      * arguments[0] {string} The selector housing an ng-app
      */
     public final static String GetLocationAbsUrl =
-            "var el = document.querySelector(arguments[0]);\n" +
-            "return angular.element(el).injector().get('$location').absUrl();";
+            "var selector = arguments[0];\n" +
+                    "  var el = document.querySelector(selector);\n" +
+                    "  if (angular.getTestability) {\n" +
+                    "    return angular.getTestability(el).\n" +
+                    "        getLocation();\n" +
+                    "  }\n" +
+                    "  return angular.element(el).injector().get('$location').absUrl();\n";
 
     /**
      * Evaluate an Angular expression in the context of a given element.
@@ -62,31 +102,51 @@ public class NgClientSideScripts {
      * @return {?Object} The result of the evaluation.
      */
     public final static String Evaluate =
-            "var element = arguments[0];\n" +
-            "var expression = arguments[1];\n" +
+            "var element = arguments[1];\n" +
+            "var expression = arguments[0];\n" +
             "return angular.element(element).scope().$eval(expression);";
 
     /**
      * Find a list of elements in the page by their angular binding.
      *
-     * arguments[0] {Element} The scope of the search.
-     * arguments[1] {string} The binding, e.g. {{cat.name}}.
+     * @param {string} binding The binding, e.g. {{cat.name}}.
+     * @param {boolean} exactMatch Whether the binding needs to be matched exactly
+     * @param {Element} using The scope of the search.
+     * @param {string} rootSelector The selector to use for the root app element.
      *
-     * @return {Array.WebElement} The elements containing the binding.
+     * @return {Array.<Element>} The elements containing the binding.
      */
     public final static String FindBindings =
-        "var using = arguments[0] || document;\n" +
-        "var binding = arguments[1];\n" +
-        "var bindings = using.getElementsByClassName('ng-binding');\n" +
-        "var matches = [];\n" +
-        "for (var i = 0; i < bindings.length; ++i) {\n" +
-        "    var bindingName = angular.element(bindings[i]).data().$binding[0].exp ||\n" +
-        "            angular.element(bindings[i]).data().$binding;\n" +
-        "    if (bindingName.indexOf(binding) != -1) {\n" +
-        "        matches.push(bindings[i]);\n" +
-        "    }\n" +
-        "}\n" +
-        "return matches;";
+        "var rootSelector = arguments[3];\n" +
+        "var using = arguments[2];\n" +
+        "var exactMatch = arguments[1];\n" +
+        "var binding = arguments[0];\n" +
+        "  var root = document.querySelector(rootSelector || 'body');\n" +
+                "  using = using || document;\n" +
+                "  if (angular.getTestability) {\n" +
+                "    return angular.getTestability(root).\n" +
+                "        findBindings(using, binding, exactMatch);\n" +
+                "  }\n" +
+                "  var bindings = using.getElementsByClassName('ng-binding');\n" +
+                "  var matches = [];\n" +
+                "  for (var i = 0; i < bindings.length; ++i) {\n" +
+                "    var dataBinding = angular.element(bindings[i]).data('$binding');\n" +
+                "    if (dataBinding) {\n" +
+                "      var bindingName = dataBinding.exp || dataBinding[0].exp || dataBinding;\n" +
+                "      if (exactMatch) {\n" +
+                "        var matcher = new RegExp('({|\\\\s|^|\\\\|)' + binding + '(}|\\\\s|$|\\\\|)');\n" +
+                "        if (matcher.test(bindingName)) {\n" +
+                "          matches.push(bindings[i]);\n" +
+                "        }\n" +
+                "      } else {\n" +
+                "        if (bindingName.indexOf(binding) != -1) {\n" +
+                "          matches.push(bindings[i]);\n" +
+                "        }\n" +
+                "      }\n" +
+                "\n" +
+                "    }\n" +
+                "  }\n" +
+                "  return matches; /* Return the whole array for webdriver.findElements. */\n";
 
     /**
      * Find input elements by model name.
@@ -97,8 +157,8 @@ public class NgClientSideScripts {
      * @return {Array.WebElement} The matching input elements.
      */
     public final static String FindInputs =
-        "var using = arguments[0] || document;\n" +
-        "var model = arguments[1];\n" +
+        "var using = arguments[1] || document;\n" +
+        "var model = arguments[0];\n" +
         "var prefixes = ['ng-', 'ng_', 'data-ng-', 'x-ng-', 'ng\\\\:'];\n" +
         "for (var p = 0; p < prefixes.length; ++p) {\n" +
         "    var selector = 'input[' + prefixes[p] + 'model=\"' + model + '\"]';\n" +
@@ -117,8 +177,8 @@ public class NgClientSideScripts {
      * @return {Array.WebElement} The matching select elements.
      */
     public final static String FindSelects =
-        "var using = arguments[0] || document;\n" +
-        "var model = arguments[1];\n" +
+        "var using = arguments[1] || document;\n" +
+        "var model = arguments[0];\n" +
         "var prefixes = ['ng-', 'ng_', 'data-ng-', 'x-ng-', 'ng\\\\:'];\n" +
         "for (var p = 0; p < prefixes.length; ++p) {\n" +
         "    var selector = 'select[' + prefixes[p] + 'model=\"' + model + '\"]';\n" +
@@ -137,8 +197,8 @@ public class NgClientSideScripts {
      * @return {Array.WebElement} The matching select elements.
      */
     public final static String FindSelectedOptions =
-        "var using = arguments[0] || document;\n" +
-        "var model = arguments[1];\n" +
+        "var using = arguments[1] || document;\n" +
+        "var model = arguments[0];\n" +
         "var prefixes = ['ng-', 'ng_', 'data-ng-', 'x-ng-', 'ng\\\\:'];\n" +
         "for (var p = 0; p < prefixes.length; ++p) {\n" +
         "    var selector = 'select[' + prefixes[p] + 'model=\"' + model + '\"] option:checked';\n" +
@@ -157,8 +217,8 @@ public class NgClientSideScripts {
      * @return {Array.WebElement} The matching textarea elements.
      */
     public final static String FindTextArea =
-        "var using = arguments[0] || document;\n" +
-        "var model = arguments[1];\n" +
+        "var using = arguments[1] || document;\n" +
+        "var model = arguments[0];\n" +
         "var prefixes = ['ng-', 'ng_', 'data-ng-', 'x-ng-', 'ng\\\\:'];\n" +
         "for (var p = 0; p < prefixes.length; ++p) {\n" +
         "    var selector = 'textarea[' + prefixes[p] + 'model=\"' + model + '\"]';\n" +
@@ -171,25 +231,55 @@ public class NgClientSideScripts {
     /**
      * Find all rows of an ng-repeat.
      *
-     * arguments[0] {Element} The scope of the search.
-     * arguments[1] {string} The text of the repeater, e.g. 'cat in cats'.
+     * @param {string} repeater The text of the repeater, e.g. 'cat in cats'.
+     * @param {boolean} exact Whether the repeater needs to be matched exactly
+     * @param {Element} using The scope of the search.
      *
-     * @return {Array.WebElement} All rows of the repeater.
+     * @return {Array.<Element>} All rows of the repeater.
      */
     public final static String FindAllRepeaterRows =
-        "var using = arguments[0] || document;\n" +
-        "var repeater = arguments[1];\n" +
-        "var rows = [];\n" +
-        "var prefixes = ['ng-', 'ng_', 'data-ng-', 'x-ng-', 'ng\\\\:'];\n" +
-        "for (var p = 0; p < prefixes.length; ++p) {\n" +
-        "    var attr = prefixes[p] + 'repeat';\n" +
-        "    var repeatElems = using.querySelectorAll('[' + attr + ']');\n" +
-        "    attr = attr.replace(/\\\\/g, '');\n" +
-        "    for (var i = 0; i < repeatElems.length; ++i) {\n" +
-        "        if (repeatElems[i].getAttribute(attr).indexOf(repeater) != -1) {\n" +
-        "            rows.push(repeatElems[i]);\n" +
-        "        }\n" +
-        "    }\n" +
-        "}\n" +
-        "return rows;";
+        "var using = arguments[2] || document;\n" +
+        "var exact = arguments[1];\n" +
+        "var repeater = arguments[0];\n" +
+        "  function repeaterMatch(ngRepeat, repeater, exact) {\n" +
+                "    if (exact) {\n" +
+                "      return ngRepeat.split(' track by ')[0].split(' as ')[0].split('|')[0].\n" +
+                "          trim() == repeater;\n" +
+                "    } else {\n" +
+                "      return ngRepeat.indexOf(repeater) != -1;\n" +
+                "    }\n" +
+                "  }\n" +
+                "\n" +
+                "  using = using || document;\n" +
+                "\n" +
+                "  var rows = [];\n" +
+                "  var prefixes = ['ng-', 'ng_', 'data-ng-', 'x-ng-', 'ng\\\\:'];\n" +
+                "  for (var p = 0; p < prefixes.length; ++p) {\n" +
+                "    var attr = prefixes[p] + 'repeat';\n" +
+                "    var repeatElems = using.querySelectorAll('[' + attr + ']');\n" +
+                "    attr = attr.replace(/\\\\/g, '');\n" +
+                "    for (var i = 0; i < repeatElems.length; ++i) {\n" +
+                "      if (repeaterMatch(repeatElems[i].getAttribute(attr), repeater, exact)) {\n" +
+                "        rows.push(repeatElems[i]);\n" +
+                "      }\n" +
+                "    }\n" +
+                "  }\n" +
+                "  for (var p = 0; p < prefixes.length; ++p) {\n" +
+                "    var attr = prefixes[p] + 'repeat-start';\n" +
+                "    var repeatElems = using.querySelectorAll('[' + attr + ']');\n" +
+                "    attr = attr.replace(/\\\\/g, '');\n" +
+                "    for (var i = 0; i < repeatElems.length; ++i) {\n" +
+                "      if (repeaterMatch(repeatElems[i].getAttribute(attr), repeater, exact)) {\n" +
+                "        var elem = repeatElems[i];\n" +
+                "        while (elem.nodeType != 8 ||\n" +
+                "            !repeaterMatch(elem.nodeValue, repeater, exact)) {\n" +
+                "          if (elem.nodeType == 1) {\n" +
+                "            rows.push(elem);\n" +
+                "          }\n" +
+                "          elem = elem.nextSibling;\n" +
+                "        }\n" +
+                "      }\n" +
+                "    }\n" +
+                "  }\n" +
+                "  return rows;";
 }
