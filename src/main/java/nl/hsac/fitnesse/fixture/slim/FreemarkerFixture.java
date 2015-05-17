8d80aa5d50452d12c91c9d550ed7ec81768ae7f4
Fried Hoeben <github@hsac.nl>
2015-05-17 10:37:06 +0200

    Simplify FreemarkerFixture using new base class

--- src/main/java/nl/hsac/fitnesse/fixture/slim/FreemarkerFixture.java
+++ src/main/java/nl/hsac/fitnesse/fixture/slim/FreemarkerFixture.java
@@ -1,17 +1,12 @@
 package nl.hsac.fitnesse.fixture.slim;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 /**
  * Fixture to generate strings using Freemarker templates. These strings can be stored in variables so the can
  * be passed as arguments to methods of other fixtures.
  * This fixture can be used using Slim's dynamic decision tables or using scripts (and scenarios).
  */
-public class FreemarkerFixture extends SlimFixture {
+public class FreemarkerFixture extends SlimFixtureWithMap {
     private final String defaultTemplate;
-
-    private final Map<String, Object> currentValues = new LinkedHashMap<String, Object>();
 
     /**
      * Creates new without default template.
@@ -35,7 +30,7 @@ public class FreemarkerFixture extends SlimFixture {
      * @return template result.
      */
     public String applyTemplate(String aTemplate) {
-        String result = getEnvironment().processTemplate(aTemplate, currentValues);
+        String result = getEnvironment().processTemplate(aTemplate, getCurrentValues());
         try {
             if (aTemplate.contains(".xml")) {
                 result = getEnvironment().getHtmlForXml(result);
@@ -48,61 +43,7 @@ public class FreemarkerFixture extends SlimFixture {
         return result;
     }
 
-    /**
-     * Stores value to be passed to template, or GET.
-     * @param value value to be passed.
-     * @param name name to use this value for.
-     */
-    public void setValueFor(Object value, String name) {
-        String cleanName = cleanupValue(name);
-        Object cleanValue = value;
-        if (value instanceof String) {
-            cleanupValue((String) value);
-        }
-        currentValues.put(cleanName, cleanValue);
-    }
-
-    /**
-     * Stores list of values to be passed to template.
-     * @param values comma separated list of values.
-     * @param name name to use this list for.
-     */
-    public void setValuesFor(String values, String name) {
-        String cleanName = cleanupValue(name);
-        String[] valueArrays = values.split("\\s*,\\s*");
-        for (int i = 0; i < valueArrays.length; i++) {
-            valueArrays[i] = cleanupValue(valueArrays[i]);
-        }
-        currentValues.put(cleanName, valueArrays);
-    }
-
-    /**
-     * Clears a values previously set.
-     * @param name value to remove.
-     * @return true if value was present.
-     */
-    public boolean clearValue(String name) {
-        String cleanName = cleanupValue(name);
-        boolean result = currentValues.containsKey(cleanName);
-        currentValues.remove(cleanName);
-        return result;
-    }
-
-    /**
-     * Clears all values previously set.
-     */
-    public void clearValues() {
-        currentValues.clear();
-    }
-
-    // methods to support usage in dynamic decision tables
-    public void reset() {
-        clearValues();
-    }
-
-    public void set(String key, Object value) {
-        setValueFor(value, key);
-    }
+    //// methods to support usage in dynamic decision tables
 
     public String get(String requestedValue) {
         String result;
@@ -114,4 +55,5 @@ public class FreemarkerFixture extends SlimFixture {
         return result;
     }
 
+    //// end: methods to support usage in dynamic decision tables
 }
