9bf66361d6b1fd650e8a01d9cbcb9d3e1fec40b5
Fried Hoeben <github@hsac.nl>
2015-05-23 18:06:38 +0200

    Add tests for trimming

--- src/test/java/nl/hsac/fitnesse/fixture/util/XMLFormatterTest.java
+++ src/test/java/nl/hsac/fitnesse/fixture/util/XMLFormatterTest.java
@@ -17,4 +17,26 @@ public class XMLFormatterTest {
         assertEquals(expected.replace("\r", ""), formatted.replace("\r", ""));
 
     }
+
+    @Test
+    public void testTrimElements() {
+        String expected = FileUtil.loadFile("GetWeatherSoapResponse.xml");
+        String formatted = FileUtil.loadFile("GetWeatherSoapResponseFormatted.xml");
+
+        String trimmed = XMLFormatter.trimElements(formatted);
+
+        assertEquals(expected, trimmed);
+        assertEquals(expected, XMLFormatter.trimElements(trimmed));
+    }
+
+    @Test
+    public void testTrim() {
+        String expected = FileUtil.loadFile("GetWeatherSoapResponse.xml")
+                            .replace("<?xml version=\"1.0\" encoding=\"UTF-8\"?>", "");
+        String formatted = FileUtil.loadFile("GetWeatherSoapResponseFormatted.xml");
+
+        String trimmed = XMLFormatter.trim(formatted);
+
+        assertEquals(expected, trimmed);
+    }
 }
