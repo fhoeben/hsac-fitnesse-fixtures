928230ca09b5feaebff37a42543b521cc58b27ae
Fried Hoeben <github@hsac.nl>
2015-05-31 21:15:22 +0200

    Add HtmlFixture to allow tags of HTML content obtained from application to be shown in wiki

---
+++ src/main/java/nl/hsac/fitnesse/fixture/slim/HtmlFixture.java
@@ -0,0 +1,30 @@
+package nl.hsac.fitnesse.fixture.slim;
+
+import nl.hsac.fitnesse.fixture.util.HtmlCleaner;
+import org.apache.commons.lang3.StringEscapeUtils;
+
+/**
+ * Fixture to facilitate working with HTML (fragments) obtained from an application being tested.
+ */
+public class HtmlFixture extends SlimFixture {
+    private final HtmlCleaner htmlCleaner = new HtmlCleaner();
+
+    /**
+     * Escapes the supplied html and places it inside a <pre></pre> block, allowing it to be shown in a wiki page.
+     * @param html HTML content to show.
+     * @return HTML tags that can be shown in wiki page.
+     */
+    public String htmlSource(String html) {
+        return "<pre>" + StringEscapeUtils.escapeHtml4(html) + "</pre>";
+    }
+
+    /**
+     * Unescapes supplied HTML content so it can be rendered inside a wiki page.
+     * @param htmlSource HTML code to display (possibly surrounded by <pre></pre> tags).
+     * @return unescaped content, enclosed in <div></div> so wiki will not escape it.
+     */
+    public String html(String htmlSource) {
+        String cleanSource = htmlCleaner.cleanupPreFormatted(htmlSource);
+        return "<div>" + StringEscapeUtils.unescapeHtml4(cleanSource) + "</div>";
+    }
+}
