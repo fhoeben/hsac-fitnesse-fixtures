e720b7c5bd6cf7c2d4d05ef14f9aaf871209cbc0
Fried Hoeben <github@hsac.nl>
2015-06-17 22:13:06 +0200

    Create acceptance test suite that will break build if these wiki pages are not green

---
+++ src/test/java/nl/hsac/fitnesse/fixture/fit/FitFixtureAcceptanceTest.java
@@ -0,0 +1,13 @@
+package nl.hsac.fitnesse.fixture.fit;
+
+import fitnesse.junit.FitNesseRunner;
+import nl.hsac.fitnesse.junit.HsacFitNesseRunner;
+import org.junit.runner.RunWith;
+
+/**
+ * Test class to run HsacAcceptanceTests.FitTests wiki suite.
+ */
+@RunWith(HsacFitNesseRunner.class)
+@FitNesseRunner.Suite("HsacAcceptanceTests.FitTests")
+public class FitFixtureAcceptanceTest {
+}
