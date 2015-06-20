33c52f3004d418d32ce99d40bc1984a12fbefa5c
Fried Hoeben <github@hsac.nl>
2015-06-20 23:32:50 +0200

    Refactor to move string -> map to JsonHelper (formerly JsonFormatter)

---
+++ src/test/java/nl/hsac/fitnesse/fixture/util/JsonHelperTest.java
@@ -0,0 +1,41 @@
+package nl.hsac.fitnesse.fixture.util;
+
+import org.junit.Test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class JsonHelperTest {
+    private JsonHelper helper = new JsonHelper();
+
+    @Test
+    public void testFormatNull() {
+        assertNull(helper.format(null));
+    }
+
+    @Test
+    public void testFormatSimple() {
+        assertEquals(
+                "{\n" +
+                        "    \"category\": \"reference\",\n" +
+                        "    \"price\": 8.95\n" +
+                        "}",
+                helper.format("{\"category\": \"reference\",\"price\": 8.95}"));
+    }
+
+    @Test
+    public void testNullToMap() {
+        assertNull(helper.jsonStringToMap(null));
+    }
+
+    @Test
+    public void testSimpleMap() {
+        Map<String, Object> expected = new LinkedHashMap<String, Object>();
+        expected.put("category", "reference");
+        expected.put("price", 8.95);
+        assertEquals(expected,
+                helper.jsonStringToMap("{\"category\": \"reference\",\"price\": 8.95}"));
+    }
+}
