154f3f2d0f7c6e8aa2853c9e718b5d35c6d60bbb
Fried Hoeben <github@hsac.nl>
2015-08-15 08:55:31 +0200

    Remove code no longer needed because pull request for Converter is incorporated (739)

--- src/main/java/nl/hsac/fitnesse/slim/converter/ObjectConverter.java
+++ src/main/java/nl/hsac/fitnesse/slim/converter/ObjectConverter.java
@@ -1,8 +1,10 @@
 package nl.hsac.fitnesse.slim.converter;
 
+import fitnesse.slim.converters.DefaultConverter;
+import fitnesse.slim.converters.ElementConverterHelper;
+
 /**
  * Slim converter to format an object of unknown static type using its actual type.
- * Needed until we use a FitNesse release including: https://github.com/unclebob/fitnesse/pull/739
  */
 public class ObjectConverter extends DefaultConverter {
     @Override
