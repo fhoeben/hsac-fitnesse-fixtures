97428ad4b1a1b1f26916ad15884cfdb4d9962a56
Fried Hoeben <github@hsac.nl>
2015-08-26 20:15:24 +0200

    Use WebDriver implementation specific way to get page source as fallback

--- src/main/java/nl/hsac/fitnesse/fixture/util/SeleniumHelper.java
+++ src/main/java/nl/hsac/fitnesse/fixture/util/SeleniumHelper.java
@@ -897,20 +897,34 @@ public class SeleniumHelper {
      * @return HTML content of current page.
      */
     public String getHtml() {
-        return (String) executeJavascript(
-                "var node = document.doctype;\n" +
-                "var docType = '';\n" +
-                "if (node) {\n" +
-                    "  docType = \"<!DOCTYPE \"\n" +
-                    "+ node.name\n" +
-                    "+ (node.publicId ? ' PUBLIC \"' + node.publicId + '\"' : '')\n" +
-                    "+ (!node.publicId && node.systemId ? ' SYSTEM' : '') \n" +
-                    "+ (node.systemId ? ' \"' + node.systemId + '\"' : '')\n" +
-                    "+ '>'; }\n" +
-                "var html = document.documentElement.outerHTML " +
-                    "|| '<html>' + document.documentElement.innerHTML + '</html>';\n" +
-                "return docType + html;"
-        );
+        String html;
+        try {
+            html = (String) executeJavascript(
+                    "var node = document.doctype;\n" +
+                            "var docType = '';\n" +
+                            "if (node) {\n" +
+                            "  docType = \"<!DOCTYPE \"\n" +
+                            "+ node.name\n" +
+                            "+ (node.publicId ? ' PUBLIC \"' + node.publicId + '\"' : '')\n" +
+                            "+ (!node.publicId && node.systemId ? ' SYSTEM' : '') \n" +
+                            "+ (node.systemId ? ' \"' + node.systemId + '\"' : '')\n" +
+                            "+ '>'; }\n" +
+                            "var html = document.documentElement.outerHTML " +
+                            "|| '<html>' + document.documentElement.innerHTML + '</html>';\n" +
+                            "return docType + html;"
+            );
+        } catch (RuntimeException e) {
+            // unable to get via Javascript
+            try {
+                // this is very WebDriver implementation dependent, so we only use as fallback
+                html = driver().getPageSource();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                // throw original exception
+                throw e;
+            }
+        }
+        return html;
     }
 
     /**
