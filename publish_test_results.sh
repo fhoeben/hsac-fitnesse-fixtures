91d2e7e9a160079d91b29794db87ed04f18f9237
Fried Hoeben <github@hsac.nl>
2015-12-22 15:02:53 +0100

    See whether we can upload test results

---
+++ publish_test_results.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+  echo -e "Starting to update gh-pages\n"
+
+  #copy data we're interested in to other place
+  cp -R target/fitnesse-results ${HOME}/acceptance-test-results
+
+  #go to home and setup git
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis"
+
+  #using token clone gh-pages branch
+  git clone --quiet --branch=gh-pages https://${GIT_HUB_TOKEN}@github.com/fhoeben/hsac-fitnesse-fixtures.git  gh-pages > /dev/null
+
+  #go into diractory and copy data we're interested in to that directory
+  cd gh-pages
+  cp -Rf ${HOME}/acceptance-test-results/* .
+
+  #add, commit and push files
+  git add -f .
+  git commit -m "Travis build ${TRAVIS_BUILD_NUMBER} pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Done magic with results\n"
+fi
