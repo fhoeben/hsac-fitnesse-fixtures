59f9d0cb0f2d0b9abc4acc1068a8b9e90fc29ee6
Fried Hoeben <github@hsac.nl>
2015-12-23 14:49:10 +0100

    Cleanup if, and allow test using current branch

--- travis_build_and_test.sh
+++ travis_build_and_test.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+# stop script on non-zero exit code by command
 set -e
 
-if [ "${TRAVIS_PULL_REQUEST}" == "false" -a "${TRAVIS_BRANCH}" == "master" -a "${TEST_TYPE}" == "acceptance-test" ]; then
-  echo -e "Create standalone.zip\n"
-  mvn compile dependency:copy-dependencies assembly:single
-fi
-
 if [ "${TEST_TYPE}" == "acceptance-test" ]; then
+  if [ "${TRAVIS_PULL_REQUEST}" == "false" -a "${TRAVIS_BRANCH}" == "travisSauceLabs" ]; then
+    echo -e "Create standalone.zip\n"
+    mvn compile dependency:copy-dependencies assembly:single
+  fi
+
   echo -e "Run acceptance test using PhantomJs on Travis server\n"
   mvn -DseleniumDriverClass=org.openqa.selenium.phantomjs.PhantomJSDriver test
-elif [ "${TEST_TYPE}" == "examples" -a "${TRAVIS_PULL_REQUEST}" == "false" -a "${TRAVIS_BRANCH}" == "master" ]; then
+elif [ "${TEST_TYPE}" == "examples" -a "${TRAVIS_PULL_REQUEST}" == "false" -a "${TRAVIS_BRANCH}" == "travisSauceLabs" ]; then
   echo -e "Run examples via SauceLabs\n"
   mvn -DfitnesseSuiteToRun=HsacExamples -DseleniumGridUrl=http://${SAUCE_USERNAME}:${SAUCE_ACCESS_KEY}@ondemand.saucelabs.com:80/wd/hub -DseleniumCapabilities="${CAPA},name:Travis ${TRAVIS_BUILD_NUMBER}-examples,build:$TRAVIS_BUILD_NUMBER" test-compile failsafe:integration-test
 fi
