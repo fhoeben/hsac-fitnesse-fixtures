38577541e51ebf7f7fc0b0d9001dc37a99673840
Fried Hoeben <github@hsac.nl>
2015-12-23 15:08:22 +0100

    Only publish from master branch

--- travis_build_and_test.sh
+++ travis_build_and_test.sh
@@ -4,7 +4,7 @@
 set -e
 
 if [ "${TEST_TYPE}" == "acceptance-test" ]; then
-  if [ "${TRAVIS_PULL_REQUEST}" == "false" -a "${TRAVIS_BRANCH}" == "travisSauceLabs" ]; then
+  if [ "${TRAVIS_PULL_REQUEST}" == "false" -a "${TRAVIS_BRANCH}" == "master" ]; then
     echo -e "Create standalone.zip\n"
     mvn compile dependency:copy-dependencies assembly:single
   fi
@@ -13,7 +13,7 @@ if [ "${TEST_TYPE}" == "acceptance-test" ]; then
   mvn -DseleniumDriverClass=org.openqa.selenium.phantomjs.PhantomJSDriver test
 
   ./travis_publish_results.sh target/fitnesse-results acceptance-test-results
-elif [ "${TEST_TYPE}" == "examples" -a "${TRAVIS_PULL_REQUEST}" == "false" -a "${TRAVIS_BRANCH}" == "travisSauceLabs" ]; then
+elif [ "${TEST_TYPE}" == "examples" -a "${TRAVIS_PULL_REQUEST}" == "false" -a "${TRAVIS_BRANCH}" == "master" ]; then
   echo -e "Run examples via SauceLabs\n"
   mvn -DfitnesseSuiteToRun=HsacExamples -DseleniumGridUrl=http://${SAUCE_USERNAME}:${SAUCE_ACCESS_KEY}@ondemand.saucelabs.com:80/wd/hub -DseleniumCapabilities="${CAPA},name:Travis ${TRAVIS_BUILD_NUMBER}-examples,build:$TRAVIS_BUILD_NUMBER" test-compile failsafe:integration-test
 
