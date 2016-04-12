bd9efcff64467838767573166ae3a118142f1fb7
Fried Hoeben <github@hsac.nl>
2016-04-12 09:59:31 +0200

    Reduce code duplication and exec empty lines

--- src/main/java/nl/hsac/fitnesse/fixture/slim/mobile/AndroidAppTest.java
+++ src/main/java/nl/hsac/fitnesse/fixture/slim/mobile/AndroidAppTest.java
@@ -1,20 +1,19 @@
 package nl.hsac.fitnesse.fixture.slim.mobile;
 
-import io.appium.java_client.*;
-
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
-import org.openqa.selenium.*;
-
 import nl.hsac.fitnesse.fixture.slim.web.BrowserTest;
 import nl.hsac.fitnesse.fixture.slim.web.annotation.TimeoutPolicy;
 import nl.hsac.fitnesse.fixture.slim.web.annotation.WaitUntil;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 
 import java.util.concurrent.TimeUnit;
 
 /**
- * Android App fixture for use with an android driver. Aimed at effortlessly testing native apps
+ * Android App fixture for use with an android driver. Aimed at effortlessly testing native apps.
  */
-
 public class AndroidAppTest extends BrowserTest {
     private int swipeSpeed = 1000;
     private int waitAfterSwipe = 0;
@@ -24,26 +23,25 @@ public class AndroidAppTest extends BrowserTest {
         setImplicitWaitForAngularTo(false);
     }
 
-    public void setImplicitWaitInSeconds(long stepDelay){
-        getSeleniumHelper().androidDriver().manage().timeouts().implicitlyWait(stepDelay, TimeUnit.SECONDS);
+    public void setImplicitWaitInSeconds(long stepDelay) {
+        getAndroidDriver().manage().timeouts().implicitlyWait(stepDelay, TimeUnit.SECONDS);
     }
 
-    public void setSwipeSpeedInMilliseconds(int swipeSpeed){
+    public void setSwipeSpeedInMilliseconds(int swipeSpeed) {
         this.swipeSpeed = swipeSpeed;
     }
 
-    public void setWaitAfterSwipe(int waitAfterSwipe){
+    public void setWaitAfterSwipe(int waitAfterSwipe) {
         this.waitAfterSwipe = waitAfterSwipe;
     }
 
     @WaitUntil(TimeoutPolicy.RETURN_FALSE)
-    public boolean tap(String locator){
-
+    public boolean tap(String locator) {
         boolean result = false;
         MobileElement element = getMobileElement(locator);
 
-        if(element != null){
-            TouchAction tap = new TouchAction(getSeleniumHelper().androidDriver()).tap(element);
+        if (element != null) {
+            TouchAction tap = new TouchAction(getAndroidDriver()).tap(element);
             tap.perform();
 
             result = true;
@@ -57,9 +55,9 @@ public class AndroidAppTest extends BrowserTest {
         boolean result = false;
         MobileElement element = getMobileElement(locator);
 
-        if(element != null){
+        if (element != null) {
             element.sendKeys(value);
-            getSeleniumHelper().androidDriver().hideKeyboard();
+            getAndroidDriver().hideKeyboard();
             result = true;
         }
 
@@ -67,71 +65,65 @@ public class AndroidAppTest extends BrowserTest {
     }
 
     @WaitUntil(TimeoutPolicy.RETURN_NULL)
-    public String valueOf(String locator){
+    public String valueOf(String locator) {
         MobileElement el = getMobileElement(locator);
         return el.getText();
     }
 
     @WaitUntil(TimeoutPolicy.RETURN_FALSE)
-    public Boolean elementDisplayed(String locator){
-        return(getMobileElement(locator).isDisplayed());
+    public Boolean elementDisplayed(String locator) {
+        return (getMobileElement(locator).isDisplayed());
     }
 
     public void swipeUp() {
-
-        Dimension size = getSeleniumHelper().androidDriver().manage().window().getSize();
+        Dimension size = getWindowSize();
         int startX = size.width / 2;
         int startY = (int) (size.height * 0.8);
         int endY = (int) (size.height * 0.2);
 
-        swipe(startX, startY, startX, endY, swipeSpeed, waitAfterSwipe);
+        swipe(startX, startY, startX, endY);
     }
 
     public void swipeDown() {
-
-        Dimension size = getSeleniumHelper().androidDriver().manage().window().getSize();
+        Dimension size = getWindowSize();
         int startX = size.width / 2;
         int startY = (int) (size.height * 0.2);
         int endY = (int) (size.height * 0.8);
 
-        swipe(startX, startY, startX, endY, swipeSpeed, waitAfterSwipe);
+        swipe(startX, startY, startX, endY);
     }
 
     public void swipeLeft() {
-
-        Dimension size = getSeleniumHelper().androidDriver().manage().window().getSize();
+        Dimension size = getWindowSize();
         int startX = (int) (size.width * 0.8);
         int endX = (int) (size.width * 0.2);
         int startY = size.height / 2;
 
-        swipe(startX, startY, endX, startY, swipeSpeed, waitAfterSwipe);
+        swipe(startX, startY, endX, startY);
     }
 
     public void swipeRight() {
-
-        Dimension size = getSeleniumHelper().androidDriver().manage().window().getSize();
+        Dimension size = getWindowSize();
         int startX = (int) (size.width * 0.2);
         int endX = (int) (size.width * 0.8);
         int startY = size.height / 2;
 
-        swipe(startX, startY, endX, startY, swipeSpeed, waitAfterSwipe);
+        swipe(startX, startY, endX, startY);
     }
 
-    public boolean swipeFrom(String direction, String locator){
+    public boolean swipeFrom(String direction, String locator) {
         boolean result = false;
-        MobileElement element = null;
-        if(getMobileElement(locator) != null) {
-            element = getMobileElement(locator);
-        }
-        if(element != null){
-            Dimension size = getSeleniumHelper().androidDriver().manage().window().getSize();
+        MobileElement element = getMobileElement(locator);
+
+        if (element != null) {
+            Dimension size = getWindowSize();
             Point elementLocation = element.getLocation();
             int startX = elementLocation.getX();
             int startY = elementLocation.getY();
             int endX;
             int endY;
 
-            switch (direction.toLowerCase().charAt(0)){
+            switch (direction.toLowerCase().charAt(0)) {
                 case 'u':
                     endX = startX;
                     endY = 0;
@@ -149,47 +141,55 @@ public class AndroidAppTest extends BrowserTest {
                     endY = startY;
                     break;
                 default:
-                   System.err.print("ERROR: " + direction + " is not a valid swipe direction.");
-                   return false;
+                    System.err.print("ERROR: " + direction + " is not a valid swipe direction.");
+                    return false;
             }
-            swipe(startX, startY, endX, endY, swipeSpeed, waitAfterSwipe);
+            swipe(startX, startY, endX, endY);
             result = true;
-        }
-        else{
+        } else {
             System.err.print("ERROR: Element not found for " + locator);
         }
         return result;
     }
 
-    private void swipe(int startX, int startY, int endX, int endY, int speed, int waitAfter){
-        getSeleniumHelper().androidDriver().swipe(startX, startY, endX, endY, speed);
+    protected void swipe(int startX, int startY, int endX, int endY) {
+        swipe(startX, startY, endX, endY, swipeSpeed, waitAfterSwipe);
+    }
+
+    protected void swipe(int startX, int startY, int endX, int endY, int speed, int waitAfter) {
+        getAndroidDriver().swipe(startX, startY, endX, endY, speed);
         waitMilliseconds(waitAfter);
     }
 
-    public void pressBackButton(){
-        getSeleniumHelper().androidDriver().navigate().back();
+    public void pressBackButton() {
+        getAndroidDriver().navigate().back();
     }
 
-    private MobileElement getMobileElement(String locator){
+    protected MobileElement getMobileElement(String locator) {
         String lookFor;
         String content;
 
-        AndroidDriver<MobileElement> driver = getSeleniumHelper().androidDriver();
-
         //assume text lookup if none is specified (convenient)
-        if(!locator.contains("=")){
+        if (!locator.contains("=")) {
             lookFor = "text";
             content = locator;
-        }
-        //Otherwise, use specified UiSelector method
-        else{
+        } else {
+            //Otherwise, use specified UiSelector method
             String[] strategy = locator.split("=");
             lookFor = strategy[0];
             content = strategy[1];
         }
 
-        return driver.findElementByAndroidUIAutomator("new UiSelector()." + lookFor + "(\"" + content + "\")");
+        String using = String.format("new UiSelector().%s(\"%s\")", lookFor, content);
+        return getAndroidDriver().findElementByAndroidUIAutomator(using);
+    }
 
+    protected Dimension getWindowSize() {
+        return getAndroidDriver().manage().window().getSize();
+    }
+
+    protected AndroidDriver<MobileElement> getAndroidDriver() {
+        return getSeleniumHelper().androidDriver();
     }
 }
 
