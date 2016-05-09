9c20508e1aaaddb19e6a924882fdd0b2c40568a7
Fried Hoeben <github@hsac.nl>
2016-05-09 16:35:56 +0200

    Simplify xpaths, and test page to see we can find buttons and elements based on their label

--- src/main/java/nl/hsac/fitnesse/fixture/util/selenium/SeleniumHelper.java
+++ src/main/java/nl/hsac/fitnesse/fixture/util/selenium/SeleniumHelper.java
@@ -122,13 +122,13 @@ public class SeleniumHelper {
                 }
             }
             if (!isInteractable(element)) {
-                element = findByXPath("//button/descendant-or-self::text()[normalize-space(.)='%s']/ancestor-or-self::button", place);
+                element = findByXPath("//button[normalize-space(descendant-or-self::text())='%s']", place);
                 if (firstFound == null) {
                     firstFound = element;
                 }
             }
             if (!isInteractable(element)) {
-                element = findByXPath("//label/descendant-or-self::text()[normalize-space(.)='%s']/ancestor-or-self::label", place);
+                element = findByXPath("//label[normalize-space(descendant-or-self::text())='%s']", place);
                 if (firstFound == null) {
                     firstFound = element;
                 }
@@ -160,13 +160,13 @@ public class SeleniumHelper {
                 }
             }
             if (!isInteractable(element)) {
-                element = findByXPath("//button/descendant-or-self::text()[contains(normalize-space(.), '%s')]/ancestor-or-self::button", place);
+                element = findByXPath("//button[contains(normalize-space(descendant-or-self::text()), '%s')]", place);
                 if (firstFound == null) {
                     firstFound = element;
                 }
             }
             if (!isInteractable(element)) {
-                element = findByXPath("//label/descendant-or-self::text()[contains(normalize-space(.), '%s')]/ancestor-or-self::label", place);
+                element = findByXPath("//label[contains(normalize-space(descendant-or-self::text()), '%s')]", place);
                 if (firstFound == null) {
                     firstFound = element;
                 }
