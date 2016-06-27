e275ea4c8b0207cd4e2db6f7ad2a807674b71efa
teunisnl <teunisnl@users.noreply.github.com>
2016-06-27 12:57:24 +0200

    Initial Idea using JsonHelper

--- src/main/java/nl/hsac/fitnesse/junit/selenium/SeleniumJsonGridDriverFactoryFactory.java
+++ src/main/java/nl/hsac/fitnesse/junit/selenium/SeleniumJsonGridDriverFactoryFactory.java
@@ -2,6 +2,7 @@ package nl.hsac.fitnesse.junit.selenium;
 
 import nl.hsac.fitnesse.fixture.slim.web.SeleniumDriverSetup;
 import nl.hsac.fitnesse.fixture.util.selenium.SeleniumHelper;
+import nl.hsac.fitnesse.fixture.Environment;
 
 import java.net.MalformedURLException;
 import java.util.LinkedHashMap;
@@ -11,12 +12,12 @@ import java.util.Map;
  * Creates a Selenium driver factory to override the configuration in the wiki.
  * This factory is configured by setting the system property 'seleniumGridUrl' AND 'seleniumCapabilities'.
  */
-public class SeleniumGridDriverFactoryFactory extends SeleniumDriverFactoryFactoryBase {
+public class SeleniumJsonGridDriverFactoryFactory extends SeleniumDriverFactoryFactoryBase {
     @Override
     public boolean willOverride() {
         return isPropertySet(SELENIUM_GRID_URL)
-                && !isPropertySet(SELENIUM_JSONCAPABILITIES)
-                && isPropertySet(SELENIUM_CAPABILITIES);
+                && !isPropertySet(SELENIUM_CAPABILITIES)
+                && isPropertySet(SELENIUM_JSONCAPABILITIES);
     }
 
     @Override
@@ -40,19 +41,9 @@ public class SeleniumGridDriverFactoryFactory extends SeleniumDriverFactoryFacto
     }
 
     protected Map<String, Object> getCapabilities() {
-        String capabilitiesString = getProperty(SELENIUM_CAPABILITIES);
+        String capabilitiesString = getProperty(SELENIUM_JSONCAPABILITIES);
         try {
-            Map<String, Object> result = new LinkedHashMap<String, Object>();
-            String[] capas = capabilitiesString.split(",");
-            for (String capa : capas) {
-                String[] kv = capa.split(":");
-                String key = kv[0].trim();
-                String value = "";
-                if (kv.length > 1) {
-                    value = capa.substring(capa.indexOf(":") + 1).trim();
-                }
-                result.put(key, value);
-            }
+            Map<String, Object> result = Environment.getInstance().getJsonHelper().jsonStringToMap(capabilitiesString);
             return result;
         } catch (Exception e) {
             throw new RuntimeException("Unable to parse Selenium capabilities: " + capabilitiesString
