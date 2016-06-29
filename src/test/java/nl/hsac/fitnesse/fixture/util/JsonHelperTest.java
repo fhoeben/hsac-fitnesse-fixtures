2faa9586f560fc88a9ff4cb104ad65a08ebfa6c4
teunisnl <teunisnl@users.noreply.github.com>
2016-06-29 14:56:19 +0200

    Using Gson as format was a bad idea Added ComplexJson to unit test

--- src/test/java/nl/hsac/fitnesse/fixture/util/JsonHelperTest.java
+++ src/test/java/nl/hsac/fitnesse/fixture/util/JsonHelperTest.java
@@ -39,4 +39,20 @@ public class JsonHelperTest {
         assertEquals(expected,
                 helper.jsonStringToMap("{\"category\": \"reference\",\"price\": 8.95}"));
     }
+
+    @Test
+    public void testComplexMap() {
+        Map<String, Object> mobileEmulation = new LinkedHashMap<String, Object>();
+        mobileEmulation.put("deviceName", "Google Nexus 5");
+
+        Map<String, Object> chromeOptions = new LinkedHashMap<String, Object>();
+        chromeOptions.put("mobileEmulation", mobileEmulation);
+
+        Map<String, Object> expected = new LinkedHashMap<String, Object>();
+        expected.put("browserName", "chrome");
+        expected.put("chromeOptions", chromeOptions);
+
+        assertEquals(expected,
+                helper.jsonStringToMap("{\"browserName\":\"chrome\",\"chromeOptions\":{\"mobileEmulation\":{\"deviceName\":\"Google Nexus 5\"}}}"));
+    }
 }
