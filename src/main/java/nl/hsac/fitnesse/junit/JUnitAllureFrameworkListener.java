edcf2eb1653135b25e75f51fdaeb79b0680a3432
tcnh <tom.heintzberger@gmail.com>
2016-07-01 16:48:08 +0200

    Page sources and screenshots attached to allure report when exceptions occur

--- src/main/java/nl/hsac/fitnesse/junit/JUnitAllureFrameworkListener.java
+++ src/main/java/nl/hsac/fitnesse/junit/JUnitAllureFrameworkListener.java
@@ -9,38 +9,32 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.apache.commons.io.FilenameUtils;
 import org.junit.Ignore;
 import org.junit.runner.Description;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunListener;
 import ru.yandex.qatools.allure.Allure;
+import ru.yandex.qatools.allure.annotations.Attachment;
 import ru.yandex.qatools.allure.config.AllureModelUtils;
-import ru.yandex.qatools.allure.events.ClearStepStorageEvent;
-import ru.yandex.qatools.allure.events.MakeAttachmentEvent;
-import ru.yandex.qatools.allure.events.TestCaseFailureEvent;
-import ru.yandex.qatools.allure.events.TestCaseFinishedEvent;
-import ru.yandex.qatools.allure.events.TestCasePendingEvent;
-import ru.yandex.qatools.allure.events.TestCaseStartedEvent;
-import ru.yandex.qatools.allure.events.TestSuiteFinishedEvent;
-import ru.yandex.qatools.allure.events.TestSuiteStartedEvent;
-import ru.yandex.qatools.allure.model.Label;
+import ru.yandex.qatools.allure.events.*;
 import ru.yandex.qatools.allure.model.Status;
-import ru.yandex.qatools.allure.model.Step;
 import ru.yandex.qatools.allure.utils.AnnotationManager;
+
 
 public class JUnitAllureFrameworkListener extends RunListener {
     private Allure lifecycle;
-    private static final String FITNESSE_RESULTS_PATH = "target/fitnesse-results";
-    private static final Pattern SCREENSHOT_PATTERN = Pattern.compile("href=\"([^\"]*.png)\"", 2);
-    private static final Pattern PAGESOURCE_PATTERN = Pattern.compile("href=\"([^\"]*.html)\"", 2);
-    private final Map<String, String> suites;
+    private static final String FITNESSE_RESULTS_PATH = "target/fitnesse-results/";
+    private static final String SCREENSHOT_EXT = "png";
+    private static final String PAGESOURCE_EXT = "html";
+    private static final Pattern SCREENSHOT_PATTERN = Pattern.compile("href=\"([^\"]*." + SCREENSHOT_EXT + ")\"");
+    private static final Pattern PAGESOURCE_PATTERN = Pattern.compile("href=\"([^\"]*." + PAGESOURCE_EXT + ")\"");
+    private final HashMap suites;
 
     public JUnitAllureFrameworkListener() {
         this.lifecycle = Allure.LIFECYCLE;
@@ -48,16 +42,16 @@ public class JUnitAllureFrameworkListener extends RunListener {
     }
 
     public void testSuiteStarted(Description description) {
-        String uid = this.generateSuiteUid(description.getDisplayName());
-        String suiteName = System.getProperty("fitnesseSuiteToRun");
+       String suiteName = System.getProperty("fitnesseSuiteToRun");
         if(null == suiteName) {
             suiteName = description.getDisplayName();
-        }
+            }
+        String uid = this.generateSuiteUid(suiteName);
 
         TestSuiteStartedEvent event = new TestSuiteStartedEvent(uid, suiteName);
         AnnotationManager am = new AnnotationManager(description.getAnnotations());
         am.update(event);
-        event.withLabels(AllureModelUtils.createTestFrameworkLabel("JUnit"), new Label[0]);
+        event.withLabels(AllureModelUtils.createTestFrameworkLabel("JUnit"));
         this.getLifecycle().fire(event);
     }
 
@@ -71,26 +65,17 @@ public class JUnitAllureFrameworkListener extends RunListener {
 
     public void testFailure(Failure failure) {
         if(failure.getDescription().isTest()) {
-            this.fireTestCaseFailure(failure.getException());
-            this.recordTestResult(failure.getDescription());
-            Matcher screenshotMatcher = SCREENSHOT_PATTERN.matcher(failure.getException().getMessage());
-            if(screenshotMatcher.find()) {
-                String pageSourceMatcher = FITNESSE_RESULTS_PATH + screenshotMatcher.group(1);
-                System.out.println("Screenshot at: " + pageSourceMatcher);
-                MakeAttachmentEvent pageSourcePath = new MakeAttachmentEvent(this.attachScreenshotFailed(pageSourceMatcher), "Screenshot", "image/png");
-                this.getLifecycle().fire(pageSourcePath);
-            } else {
-                System.out.println("No match for " + SCREENSHOT_PATTERN.toString() + " in " + failure.getException().getMessage());
-            }
+            Throwable exception = failure.getException();
 
-            Matcher pageSourceMatcher1 = PAGESOURCE_PATTERN.matcher(failure.getException().getMessage());
-            if(pageSourceMatcher1.find()) {
-                String pageSourcePath1 = FITNESSE_RESULTS_PATH + pageSourceMatcher1.group(1);
-                System.out.println("PageSource at: " + pageSourcePath1);
-                MakeAttachmentEvent pageSourceEvent = new MakeAttachmentEvent(this.attachScreenshotFailed(pageSourcePath1), "Page Source", "text/html");
-                this.getLifecycle().fire(pageSourceEvent);
-                pageSourceEvent.process(new Step());
-            }
+            List<Pattern> patterns = new ArrayList<>();
+            patterns.add(SCREENSHOT_PATTERN);
+            patterns.add(PAGESOURCE_PATTERN);
+            processAttachments(exception, patterns);
+
+            this.fireTestCaseFailure(exception);
+            this.recordTestResult(failure.getDescription());
+
+
         } else {
             this.startFakeTestCase(failure.getDescription());
             this.fireTestCaseFailure(failure.getException());
@@ -118,10 +103,8 @@ public class JUnitAllureFrameworkListener extends RunListener {
     }
 
     public void testRunFinished(Result result) {
-        Iterator i$ = this.getSuites().values().iterator();
 
-        while(i$.hasNext()) {
-            String uid = (String)i$.next();
+        for (String uid : this.getSuites().values()) {
             this.testSuiteFinished(uid);
         }
 
@@ -142,11 +125,11 @@ public class JUnitAllureFrameworkListener extends RunListener {
             this.testSuiteStarted(suiteDescription);
         }
 
-        return (String)this.getSuites().get(suiteName);
+        return this.getSuites().get(suiteName);
     }
 
     public String getIgnoredMessage(Description description) {
-        Ignore ignore = (Ignore)description.getAnnotation(Ignore.class);
+        Ignore ignore = description.getAnnotation(Ignore.class);
         return ignore != null && !ignore.value().isEmpty()?ignore.value():"Test ignored (without reason)!";
     }
 
@@ -191,28 +174,42 @@ public class JUnitAllureFrameworkListener extends RunListener {
         this.testFinished(description);
     }
 
-    public byte[] attachScreenshotFailed(String screenshotPath) {
-        Path path = Paths.get(screenshotPath, new String[0]);
+    protected void processAttachments(Throwable ex, List<Pattern> patterns){
 
-        byte[] data;
-        try {
-            data = Files.readAllBytes(path);
-        } catch (IOException var5) {
-            System.err.println("File not found: " + screenshotPath);
-            data = null;
+        for(Pattern pattern : patterns){
+            Matcher patternMatcher = pattern.matcher(ex.getMessage());
+            if(patternMatcher.find()) {
+                String filePath = FITNESSE_RESULTS_PATH + patternMatcher.group(1);
+                String attName;
+                String ext = FilenameUtils.getExtension(Paths.get(filePath).toString());
+                if(ext.equalsIgnoreCase(SCREENSHOT_EXT)){
+                    attName = "Page Screenshot";
+                } else if(ext.equalsIgnoreCase(PAGESOURCE_EXT)){
+                    attName = "Page Source";
+                } else{
+                    attName = "Attachment";
+                }
+                System.out.println("Attachment found at: " + filePath);
+                attachFile(filePath, attName);
+                //MakeAttachmentEvent event = new MakeAttachmentEvent(fileToAttach(filePath),"Attachment", "image/png");
+                //this.getLifecycle().fire(event);
+            } else {
+                System.out.println("No match for " + pattern.toString() + " in " + ex.getMessage());
+            }
         }
 
-        return data;
+
     }
 
-    public byte[] attachPageSourceFailed(String pageSourcePath) {
-        Path path = Paths.get(pageSourcePath, new String[0]);
+    @Attachment(value = "{1}")
+    protected byte[] attachFile(String filePath, String attName) {
+        Path path = Paths.get(filePath);
 
         byte[] data;
         try {
             data = Files.readAllBytes(path);
         } catch (IOException var5) {
-            System.err.println("File not found: " + pageSourcePath);
+            System.err.println(attName + " not found: " + path.toString());
             data = null;
         }
 
