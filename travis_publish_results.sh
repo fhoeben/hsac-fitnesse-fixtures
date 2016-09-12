19220a02e37f106df6738bd9fa4cfa803e58e14a
Fried Hoeben <github@hsac.nl>
2016-09-12 17:14:19 +0200

    No need to copy history, we just want latest information

--- travis_publish_results.sh
+++ travis_publish_results.sh
@@ -13,7 +13,7 @@ if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
   git config --global user.name "Travis"
 
   #using token clone gh-pages branch
-  git clone --quiet --branch=gh-pages https://${GIT_HUB_TOKEN}@github.com/fhoeben/hsac-fitnesse-fixtures.git  gh-pages > /dev/null
+  git clone --quiet --branch=gh-pages --depth 1 https://${GIT_HUB_TOKEN}@github.com/fhoeben/hsac-fitnesse-fixtures.git  gh-pages > /dev/null
 
   #go into directory, remove files from previous build and copy data we're interested in to that directory
   cd gh-pages
