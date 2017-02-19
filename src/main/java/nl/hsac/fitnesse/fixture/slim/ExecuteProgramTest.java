7ea27f41cd53c30d27c03ceeed020572ead33046
Fried Hoeben <github@hsac.nl>
2017-02-19 21:26:35 +0100

    Add JavaDoc to execute program test

--- src/main/java/nl/hsac/fitnesse/fixture/slim/ExecuteProgramTest.java
+++ src/main/java/nl/hsac/fitnesse/fixture/slim/ExecuteProgramTest.java
@@ -17,22 +17,37 @@ public class ExecuteProgramTest extends SlimFixtureWithMap {
     private List<String> arguments = new ArrayList<>();
     private ProgramResponse response = new ProgramResponse();
 
-    public void setTimeoutOfSeconds(int timeoutSeconds) {
-        timeout = timeoutSeconds * 1000;
-    }
-
+    /**
+     * @param timeout max time for test to wait for program to finish, in milliseconds.
+     */
     public void setTimeoutOfMilliseconds(int timeout) {
         this.timeout = timeout;
     }
 
+    /**
+     * @param timeoutSeconds max time for test to wait for program to finish, in seconds.
+     */
+    public void setTimeoutOfSeconds(int timeoutSeconds) {
+        setTimeoutOfMilliseconds(timeoutSeconds * 1000);
+    }
+
+    /**
+     * @return max time for test to wait for program to finish, in milliseconds.
+     */
     public int timeout() {
         return timeout;
     }
 
+    /**
+     * @param workingDirectory working directory when starting program.
+     */
     public void setWorkingDirectory(String workingDirectory) {
         this.workingDirectory = workingDirectory;
     }
 
+    /**
+     * @param arguments arguments for program.
+     */
     public void setAsArguments(List<String> arguments) {
         if (arguments == null) {
             this.arguments = new ArrayList<>();
@@ -41,6 +56,10 @@ public class ExecuteProgramTest extends SlimFixtureWithMap {
         }
     }
 
+    /**
+     * @param argument argument for program.
+     * @param index index (zero-based) for argument.
+     */
     public void setAsArgument(String argument, int index) {
         while (arguments.size() <= index) {
             arguments.add("");
@@ -48,10 +67,12 @@ public class ExecuteProgramTest extends SlimFixtureWithMap {
         arguments.set(index, argument);
     }
 
-    public List<String> getArguments() {
-        return arguments;
-    }
-
+    /**
+     * Executes command.
+     * @param command executable to start.
+     * @return true.
+     * @throws RuntimeException when unable to start program, or on timeout.
+     */
     public boolean execute(String command) {
         response = new ProgramResponse();
         if (!getCurrentValues().isEmpty()) {
@@ -69,26 +90,46 @@ public class ExecuteProgramTest extends SlimFixtureWithMap {
         return true;
     }
 
+    /**
+     * @return exit code of program.
+     */
     public Integer exitCode() {
         return response.getExitCode();
     }
 
+    /**
+     * @return formatted standard output of program.
+     */
     public String standardOut() {
         return formatOutput(rawStandardOut());
     }
 
+    /**
+     * @return formatted standard error of program.
+     */
     public String standardError() {
         return rawStandardError();
     }
 
+    /**
+     * @return un-formatted standard output of program.
+     */
     public String rawStandardOut() {
         return response.getStdOut();
     }
 
+    /**
+     * @return un-formatted standard error of program.
+     */
     public String rawStandardError() {
         return response.getStdErr();
     }
 
+    /**
+     * Formats output of program for display in wiki.
+     * @param output output of program.
+     * @return formatted output.
+     */
     protected String formatOutput(String output) {
         String result;
         if (StringUtils.isEmpty(output)) {
