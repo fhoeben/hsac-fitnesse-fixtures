e7f6fb409007e88931822a658c1493af077df40e
Fried Hoeben <github@hsac.nl>
2017-05-28 22:09:51 +0200

    Simplify FundaTest fixture

--- src/main/java/nl/hsac/fitnesse/sample/FundaTest.java
+++ src/main/java/nl/hsac/fitnesse/sample/FundaTest.java
@@ -4,25 +4,12 @@ import nl.hsac.fitnesse.fixture.slim.web.BrowserTest;
 import nl.hsac.fitnesse.fixture.slim.web.annotation.TimeoutPolicy;
 import nl.hsac.fitnesse.fixture.slim.web.annotation.WaitUntil;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
  * Fixture class customized to test funda.nl.
  */
 public class FundaTest extends BrowserTest {
-    private static final Pattern COUNT_PATTERN = Pattern.compile("(\\d+)");
     @WaitUntil(TimeoutPolicy.RETURN_NULL)
-    public Integer numberOfPhotos() {
-        Integer count = null;
-        String countText = valueOf("xpath=//span[@class='object-media-teaser-count']");
-        if (countText != null) {
-            Matcher m = COUNT_PATTERN.matcher(countText);
-            if (m.matches()) {
-                String counterStr = m.group(1);
-                count = Integer.valueOf(counterStr);
-            }
-        }
-        return count;
+    public String numberOfPhotos() {
+        return valueOf("xpath=//span[@class='object-media-teaser-count']");
     }
 }
