0cef138217fda8a141e406bc21acd1812c5ba2a7
Teunis van Vliet <tvanvliet@schubergphilis.com>
2017-06-02 12:05:25 +0200

    Added spaces

--- src/main/java/nl/hsac/fitnesse/fixture/util/selenium/SeleniumHelper.java
+++ src/main/java/nl/hsac/fitnesse/fixture/util/selenium/SeleniumHelper.java
@@ -989,6 +989,12 @@ public class SeleniumHelper {
         getActions().dragAndDrop(source, target).perform();
     }
 
+    /**
+     * Simulates click with an offset
+     * @param element element to click on
+     * @param xOffset amount pixels to offset measured of the left side
+     * @param yOffset amount pixels to offset measured of the top side
+     */
     public void clickWithOffset(WebElement element, int xOffset, int yOffset) {
         getActions().moveToElement(element, xOffset, yOffset ).click().perform();
     }
