a64a61f433486dfc51e86231a4c21d0669d89849
Fried Hoeben <github@hsac.nl>
2017-09-01 23:01:12 +0200

    Separate Bys for exact and partial container matching

--- src/main/java/nl/hsac/fitnesse/fixture/util/selenium/by/ContainerBy.java
+++ src/main/java/nl/hsac/fitnesse/fixture/util/selenium/by/ContainerBy.java
@@ -15,11 +15,40 @@ public class ContainerBy {
         return new Heuristic(containerName);
     }
 
+    /**
+     * By to find container by its full name.
+     * @param containerName name of container.
+     * @return By finding containers matching the name.
+     */
+    public static By exact(String containerName) {
+        return new Exact(containerName);
+    }
+
+    /**
+     * By to find container by its partial name.
+     * @param containerName name of container.
+     * @return By finding containers matching the name.
+     */
+    public static By partial(String containerName) {
+        return new Partial(containerName);
+    }
+
     public static class Heuristic extends HeuristicBy {
         public Heuristic(String container) {
+            super(exact(container), partial(container));
+        }
+    }
+
+    public static class Exact extends HeuristicBy {
+        public Exact(String container) {
             super(new XPathBy(".//fieldset[.//legend/text()[normalized(.) = '%s']]", container),
-                    AriaLabelBy.exact(container),
-                    new XPathBy(".//fieldset[.//legend/text()[contains(normalized(.), '%s')]]", container),
+                    AriaLabelBy.exact(container));
+        }
+    }
+
+    public static class Partial extends HeuristicBy {
+        public Partial(String container) {
+            super(new XPathBy(".//fieldset[.//legend/text()[contains(normalized(.), '%s')]]", container),
                     AriaLabelBy.partial(container));
         }
     }
