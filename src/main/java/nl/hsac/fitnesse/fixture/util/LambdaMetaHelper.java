827348be15c8c2e49a4be7828d2f76a2e5dad03b
Fried Hoeben <github@hsac.nl>
2017-09-19 18:53:05 +0200

    Add JavaDoc

--- src/main/java/nl/hsac/fitnesse/fixture/util/LambdaMetaHelper.java
+++ src/main/java/nl/hsac/fitnesse/fixture/util/LambdaMetaHelper.java
@@ -16,16 +16,40 @@ import static java.lang.invoke.MethodType.methodType;
  */
 public class LambdaMetaHelper {
 
+    /**
+     * Gets no-arg constructor as Supplier.
+     * @param clazz class to get constructor for.
+     * @param <T> clazz.
+     * @return supplier.
+     */
     public <T> Supplier<T> getConstructor(Class<? extends T> clazz) {
         return getConstructorAs(Supplier.class, "get", clazz);
     }
 
+    /**
+     * Gets single arg constructor as Function.
+     * @param clazz class to get constructor for.
+     * @param arg constructor argument type.
+     * @param <T> clazz.
+     * @param <A> argument class.
+     * @return function.
+     */
     public <T, A> Function<A, T> getConstructor(Class<? extends T> clazz, Class<A> arg) {
         return getConstructorAs(Function.class, "apply", clazz, arg);
     }
 
-    public <T, U, R> BiFunction<T, U, R> getConstructor(Class<? extends R> clazz, Class<?>... args) {
-        return getConstructorAs(BiFunction.class, "apply", clazz, args);
+    /**
+     * Gets two arg constructor as BiFunction.
+     * @param clazz class to get constructor for.
+     * @param arg1 first argument class.
+     * @param arg2 second argument class.
+     * @param <T> clazz.
+     * @param <A1> first argument class
+     * @param <A2> second argument class.
+     * @return bifunction.
+     */
+    public <A1, A2, T> BiFunction<A1, A2, T> getConstructor(Class<? extends T> clazz, Class<A1> arg1, Class<A2> arg2) {
+        return getConstructorAs(BiFunction.class, "apply", clazz, new Class<?>[] {arg1, arg2});
     }
 
     protected <T, R> T getConstructorAs(Class<T> targetClass, String methodName, Class<? extends R> clazz, Class<?>... args) {
