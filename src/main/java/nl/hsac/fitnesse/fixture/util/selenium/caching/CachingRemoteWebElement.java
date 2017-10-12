230a876ec4aa001f03172b83d8a62bf98af12b16
Fried Hoeben <github@hsac.nl>
2017-10-12 14:12:27 +0200

    Also cache other properties

--- src/main/java/nl/hsac/fitnesse/fixture/util/selenium/caching/CachingRemoteWebElement.java
+++ src/main/java/nl/hsac/fitnesse/fixture/util/selenium/caching/CachingRemoteWebElement.java
@@ -1,5 +1,8 @@
 package nl.hsac.fitnesse.fixture.util.selenium.caching;
 
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.Rectangle;
 import org.openqa.selenium.remote.RemoteWebElement;
 
 import java.util.HashMap;
@@ -10,10 +13,15 @@ import java.util.function.Function;
  * Our version of RemoteWebElement, optimizing calls to the server to obtain values.
  */
 public class CachingRemoteWebElement extends RemoteWebElement {
+    private final BooleanCache isSelectedCache = new BooleanCache(super::isSelected);
     private final BooleanCache isDisplayedCache = new BooleanCache(super::isDisplayed);
     private final BooleanCache isEnabledCache = new BooleanCache(super::isEnabled);
     private final ObjectCache<String> tagNameCache = new ObjectCache<>(super::getTagName);
     private final ObjectCache<String> textCache = new ObjectCache<>(super::getText);
+    private final ObjectCache<Point> locationCache = new ObjectCache<>(super::getLocation);
+    private final ObjectCache<Dimension> sizeCache = new ObjectCache<>(super::getSize);
+    private final ObjectCache<Rectangle> rectCache = new ObjectCache<>(super::getRect);
+
     private final Map<String, ObjectCache<String>> attributesCache = new HashMap<>();
     private final Function<String, ObjectCache<String>> attributeCacheCreationFunction = x -> new ObjectCache(() -> super.getAttribute(x));
 
@@ -21,6 +29,11 @@ public class CachingRemoteWebElement extends RemoteWebElement {
         if (element != null) {
             setId(element.getId());
         }
+    }
+
+    @Override
+    public boolean isSelected() {
+        return isSelectedCache.getBooleanValue();
     }
 
     @Override
@@ -41,6 +54,21 @@ public class CachingRemoteWebElement extends RemoteWebElement {
     @Override
     public String getText() {
         return textCache.getValue();
+    }
+
+    @Override
+    public Point getLocation() {
+        return locationCache.getValue();
+    }
+
+    @Override
+    public Dimension getSize() {
+        return sizeCache.getValue();
+    }
+
+    @Override
+    public Rectangle getRect() {
+        return rectCache.getValue();
     }
 
     @Override
