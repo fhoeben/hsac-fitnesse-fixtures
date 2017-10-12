e95613d34cca9838dbcbfd80974ed0dfc2363e37
Fried Hoeben <github@hsac.nl>
2017-10-12 14:02:00 +0200

    Also cache element attributes

--- src/main/java/nl/hsac/fitnesse/fixture/util/selenium/caching/CachingRemoteWebElement.java
+++ src/main/java/nl/hsac/fitnesse/fixture/util/selenium/caching/CachingRemoteWebElement.java
@@ -2,6 +2,10 @@ package nl.hsac.fitnesse.fixture.util.selenium.caching;
 
 import org.openqa.selenium.remote.RemoteWebElement;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
 /**
  * Our version of RemoteWebElement, optimizing calls to the server to obtain values.
  */
@@ -10,6 +14,8 @@ public class CachingRemoteWebElement extends RemoteWebElement {
     private final BooleanCache isEnabledCache = new BooleanCache(super::isEnabled);
     private final ObjectCache<String> tagNameCache = new ObjectCache<>(super::getTagName);
     private final ObjectCache<String> textCache = new ObjectCache<>(super::getText);
+    private final Map<String, ObjectCache<String>> attributesCache = new HashMap<>();
+    private final Function<String, ObjectCache<String>> attributeCacheCreationFunction = x -> new ObjectCache(() -> super.getAttribute(x));
 
     public CachingRemoteWebElement(RemoteWebElement element) {
         if (element != null) {
@@ -35,5 +41,11 @@ public class CachingRemoteWebElement extends RemoteWebElement {
     @Override
     public String getText() {
         return textCache.getValue();
+    }
+
+    @Override
+    public String getAttribute(String name) {
+        ObjectCache<String> cache = attributesCache.computeIfAbsent(name, attributeCacheCreationFunction);
+        return cache.getValue();
     }
 }
