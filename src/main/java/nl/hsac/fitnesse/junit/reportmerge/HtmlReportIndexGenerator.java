ecd72f67e538a684cadb94162da57daada75c2fa
Fried Hoeben <github@hsac.nl>
2018-11-10 17:01:58 +0100

    Add pie charts

--- src/main/java/nl/hsac/fitnesse/junit/reportmerge/HtmlReportIndexGenerator.java
+++ src/main/java/nl/hsac/fitnesse/junit/reportmerge/HtmlReportIndexGenerator.java
@@ -7,7 +7,11 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.text.NumberFormat;
+import java.util.Collection;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -22,6 +26,9 @@ import static nl.hsac.fitnesse.junit.reportmerge.TestReportHtml.PASS_STATUS;
  * This is useful to generate a combined result page when multiple suites were run separately (e.g. in parallel).
  */
 public class HtmlReportIndexGenerator {
+    private static final String TESTCOUNT_CHART_ID = "countPie";
+    private static final String RUNTIME_CHART_ID = "runtimePie";
+    private static final String STATUS_CHART_ID = "statusPie";
     private final NumberFormat nf = NumberFormat.getIntegerInstance();
 
     public static void main(String[] arguments) throws IOException {
@@ -92,8 +99,13 @@ public class HtmlReportIndexGenerator {
         String firstRunCssDir = firstTestPage.getDirectory();
         pw.write("<html><head><meta http-equiv='Content-Type' content='text/html;charset=UTF-8'/><link rel='stylesheet' type='text/css' href='");
         pw.write(firstRunCssDir);
-        pw.write("/css/fitnesse.css'/></head>");
-        pw.write("<body>");
+        pw.write("/css/fitnesse.css'/>");
+        writeExtraHeaderContent(pw, htmls);
+        pw.write("</head><body>");
+    }
+
+    protected void writeExtraHeaderContent(PrintWriter pw, List<TestReportHtml> htmls) {
+        pw.write("<script type='text/javascript' src='https://www.gstatic.com/charts/loader.js'></script>");
     }
 
     protected void writeFooter(PrintWriter pw, List<TestReportHtml> htmls) {
@@ -107,20 +119,26 @@ public class HtmlReportIndexGenerator {
 
     protected void writeOverviewSection(PrintWriter pw, List<TestReportHtml> htmls) {
         writeOverviewGraph(pw, htmls);
+
         List<TestReportHtml> overviewPages = filterBy(htmls, TestReportHtml::isOverviewPage);
         writeSection(pw, "Overview Pages", overviewPages);
     }
 
     protected void writeOverviewGraph(PrintWriter pw, List<TestReportHtml> htmls) {
+        pw.write("<div style='width:100%;'>");
+
         List<TestReportHtml> testHtmls = filterBy(htmls,
                 x -> !x.isOverviewPage()
                         && !NO_TEST_STATUS.equals(x.getStatus()));
-        pw.write("<table style=\"width:100%;text-align:center;\"><tr>");
+        pw.write("<table style='width:100%;text-align:center;'><tr>");
         writeGraphCell(pw, ERROR_STATUS, testHtmls);
         writeGraphCell(pw, FAIL_STATUS, testHtmls);
         writeGraphCell(pw, IGNORE_STATUS, testHtmls);
         writeGraphCell(pw, PASS_STATUS, testHtmls);
         pw.write("</tr></table>");
+
+        writePieChartsElements(pw, htmls);
+        pw.write("</div>");
     }
 
     protected void writeGraphCell(PrintWriter pw, String status, List<TestReportHtml> testHtmls) {
@@ -131,6 +149,78 @@ public class HtmlReportIndexGenerator {
             String cell = String.format("<td class=\"%s\" style=\"width:%s%%;\">%s</td>", status, pct, count);
             pw.write(cell);
         }
+    }
+
+    protected void writePieChartsElements(PrintWriter pw, List<TestReportHtml> htmls) {
+        pw.write("<div style='display:flex;flex-wrap:wrap;justify-content:center;'>");
+        pw.write("<div id='");
+        pw.write(STATUS_CHART_ID);
+        pw.write("'></div>");
+        pw.write("<div id='");
+        pw.write(TESTCOUNT_CHART_ID);
+        pw.write("'></div>");
+        pw.write("<div id='");
+        pw.write(RUNTIME_CHART_ID);
+        pw.write("'></div>");
+        writeChartGenerators(pw, htmls);
+        pw.write("</div>");
+    }
+
+    protected void writeChartGenerators(PrintWriter pw, List<TestReportHtml> htmls) {
+        pw.write("<script type='text/javascript'>" +
+                "if(window.google){google.charts.load('current',{'packages':['corechart']});" +
+                "google.charts.setOnLoadCallback(drawChart);" +
+                "function drawChart() {");
+
+        Map<String, Long> displayedStatus = getStatusMap(htmls);
+
+        writePieChartGenerator(pw, "Status", STATUS_CHART_ID,
+                ",slices:[{color:'#ffffaa'},{color:'#FF6666'},{color:'orange'},{color:'#28B463'},{color:'lightgray'}]",
+                r -> r.getKey(), r -> r.getValue(), displayedStatus.entrySet());
+
+        List<Map.Entry<String, Long>> counts = sortBy(
+                filterBy(htmls,
+                        r -> !r.isOverviewPage()).stream()
+                        .collect(Collectors.groupingBy(TestReportHtml::getRunName, Collectors.counting())).entrySet(),
+                r -> r.getKey());
+        writePieChartGenerator(pw, "Count", TESTCOUNT_CHART_ID, "",
+                r -> r.getKey(), r -> r.getValue(), counts);
+
+        List<TestReportHtml> overviewPages = sortBy(
+                filterBy(htmls, TestReportHtml::isOverviewPage),
+                r -> r.getRunName());
+        writePieChartGenerator(pw, "Time", RUNTIME_CHART_ID, "",
+                r -> r.getRunName(), r -> r.getTime(), overviewPages);
+        pw.write("}}</script>");
+    }
+
+    protected <T> void writePieChartGenerator(PrintWriter pw,
+                                              String title,
+                                              String chartElementId,
+                                              String slicesProperty,
+                                              Function<T, String> keyFunction,
+                                              Function<T, Number> valueFunction,
+                                              Iterable<T> htmls) {
+        StringBuilder data = new StringBuilder("[['Group',''],");
+        htmls.forEach(r -> {
+            data.append("['");
+            data.append(keyFunction.apply(r));
+            data.append("',");
+            data.append(valueFunction.apply(r));
+            data.append("],");
+        });
+        data.append("]");
+        String dataArray = data.toString();
+
+        pw.write("new google.visualization.PieChart(document.getElementById('");
+        pw.write(chartElementId);
+        pw.write("')).draw(google.visualization.arrayToDataTable(");
+        pw.write(dataArray);
+        pw.write("),{title:'");
+        pw.write(title);
+        pw.write("',sliceVisibilityThreshold:0,pieSliceTextStyle:{color:'black'}");
+        pw.write(slicesProperty);
+        pw.write("});");
     }
 
     protected void writeTestResultsSection(PrintWriter pw, List<TestReportHtml> htmls) {
@@ -190,12 +280,34 @@ public class HtmlReportIndexGenerator {
         pw.write("\">");
         pw.write(testName);
         pw.write("</a></td><td>");
-        pw.write(time < 0? "unknown" : nf.format(time));
+        pw.write(time < 0 ? "unknown" : nf.format(time));
         pw.write("</td></tr>");
+    }
+
+    protected Map<String, Long> getStatusMap(List<TestReportHtml> htmls) {
+        Map<String, Long> statuses = filterBy(htmls, r -> !r.isOverviewPage()).stream()
+                .collect(Collectors.groupingBy(TestReportHtml::getStatus, Collectors.counting()));
+
+        Map<String, Long> displayedStatus = new LinkedHashMap<>();
+        addStatusEntry(ERROR_STATUS, statuses, displayedStatus);
+        addStatusEntry(FAIL_STATUS, statuses, displayedStatus);
+        addStatusEntry(IGNORE_STATUS, statuses, displayedStatus);
+        addStatusEntry(PASS_STATUS, statuses, displayedStatus);
+        addStatusEntry(NO_TEST_STATUS, statuses, displayedStatus);
+        return displayedStatus;
+    }
+
+    protected void addStatusEntry(String status, Map<String, Long> statuses, Map<String, Long> displayedStatus) {
+        displayedStatus.put(status, statuses.getOrDefault(status, 0L));
     }
 
     protected static List<TestReportHtml> filterByStatus(List<TestReportHtml> htmls, String desiredStatus) {
         return filterBy(htmls, x -> desiredStatus.equals(x.getStatus()));
+    }
+
+    protected static <T> List<T> sortBy(Collection<T> values, Function<T, ? extends Comparable> function) {
+        return values.stream().sorted((o1, o2) -> function.apply(o1).compareTo(function.apply(o2)))
+                .collect(Collectors.toList());
     }
 
     protected static <T> List<T> filterBy(List<T> list, Predicate<T> predicate) {
