2c29f4b10c6226f28f41a84269d1a92fdac10ae3
Tom Heintzberger <tom.heintzberger@gmail.com>
2020-08-26 08:34:15 +0200

    Add support for WAI-Aria design pattern tables to Browser Test (#341)
    Add support for WAI-Aria design pattern tables to Browser Test

---
+++ src/main/java/nl/hsac/fitnesse/fixture/util/selenium/by/ariagrid/Value.java
@@ -0,0 +1,76 @@
+package nl.hsac.fitnesse.fixture.util.selenium.by.ariagrid;
+
+import nl.hsac.fitnesse.fixture.util.selenium.by.SingleElementOrNullBy;
+import nl.hsac.fitnesse.fixture.util.selenium.by.ValueOfBy;
+import nl.hsac.fitnesse.fixture.util.selenium.by.XPathBy;
+import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebElement;
+
+import static nl.hsac.fitnesse.fixture.util.selenium.by.AriaGridBy.getXPathForColumnInRowByValueInOtherColumn;
+import static nl.hsac.fitnesse.fixture.util.selenium.by.AriaGridBy.getXPathForColumnIndex;
+import static nl.hsac.fitnesse.fixture.util.selenium.by.AriaGridBy.getXPathForHeaderRowByHeaders;
+
+public abstract class Value extends SingleElementOrNullBy {
+    public static class AtCoordinates extends Value {
+        private final int columnIndex;
+        private final int rowIndex;
+
+        public AtCoordinates(int columnIndex, int rowIndex) {
+            this.columnIndex = columnIndex;
+            this.rowIndex = rowIndex;
+        }
+
+        @Override
+        public WebElement findElement(SearchContext context) {
+            String row = Integer.toString(rowIndex);
+            String column = Integer.toString(columnIndex);
+            return getValueByXPath(context, "(.//div[@role='row'][boolean(span[@role='cell'])])[%s]/span[@role='cell'][%s]", row, column);
+        }
+    }
+
+    public static class OfInRowNumber extends Value {
+        private final String requestedColumnName;
+        private final int rowIndex;
+
+        public OfInRowNumber(String requestedColumnName, int rowIndex) {
+            this.requestedColumnName = requestedColumnName;
+            this.rowIndex = rowIndex;
+        }
+
+        @Override
+        public WebElement findElement(SearchContext context) {
+            String headerXPath = getXPathForHeaderRowByHeaders(requestedColumnName);
+            String columnXPath = String.format("((.//div[@role='table'][./%1$s])[last()]//div[@role='row'][boolean(span[@role='cell'])])[%2$s]/span[@role='cell']", headerXPath, rowIndex);
+            return valueInRow(context, columnXPath, requestedColumnName);
+        }
+    }
+
+    public static class OfInRowWhereIs extends Value {
+        private final String requestedColumnName;
+        private final String selectOnColumn;
+        private final String selectOnValue;
+
+        public OfInRowWhereIs(String requestedColumnName, String selectOnValue, String selectOnColumn) {
+            this.requestedColumnName = requestedColumnName;
+            this.selectOnColumn = selectOnColumn;
+            this.selectOnValue = selectOnValue;
+        }
+
+        @Override
+        public WebElement findElement(SearchContext context) {
+            String columnXPath = getXPathForColumnInRowByValueInOtherColumn(selectOnValue, selectOnColumn, requestedColumnName);
+            return valueInRow(context, columnXPath, requestedColumnName);
+        }
+    }
+
+    protected WebElement valueInRow(SearchContext context, String columnXPath, String requestedColumnName) {
+        String requestedIndex = getXPathForColumnIndex(requestedColumnName);
+        return getValueByXPath(context, "%s[%s]", columnXPath, requestedIndex);
+    }
+
+    protected WebElement getValueByXPath(SearchContext context, String xpathPattern, String... params) {
+        By xPathBy = new XPathBy(xpathPattern, params);
+        return new ValueOfBy(xPathBy).findElement(context);
+    }
+}
