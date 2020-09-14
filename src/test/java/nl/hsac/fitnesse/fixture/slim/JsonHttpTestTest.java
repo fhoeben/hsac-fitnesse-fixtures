4e250004c5e6c2916a3af544f3323b62975a9e68
Bas Vegter <bas.vegter@praegus.nl>
2020-09-14 15:20:32 +0200

    Revert all changes to JsonHttpTestTest

--- src/test/java/nl/hsac/fitnesse/fixture/slim/JsonHttpTestTest.java
+++ src/test/java/nl/hsac/fitnesse/fixture/slim/JsonHttpTestTest.java
@@ -6,7 +6,8 @@ import org.junit.Test;
 
 import static junit.framework.TestCase.assertTrue;
 import static nl.hsac.fitnesse.fixture.slim.HttpTestTest.checkCall;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 /**
  * Tests JsonHttpTest.
