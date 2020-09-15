e1fdbcab056aa3bf99664b9426e126af1f5e3ed1
Tcnh <tom.heintzberger@gmail.com>
2020-09-15 15:35:15 +0200

    optimized imports

--- src/main/java/nl/hsac/fitnesse/fixture/util/selenium/driverfactory/LocalDriverFactory.java
+++ src/main/java/nl/hsac/fitnesse/fixture/util/selenium/driverfactory/LocalDriverFactory.java
@@ -1,12 +1,10 @@
 package nl.hsac.fitnesse.fixture.util.selenium.driverfactory;
 
-import com.galenframework.generator.raycast.EdgesContainer;
 import nl.hsac.fitnesse.fixture.slim.SlimFixtureException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
