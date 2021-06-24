842b5be432799a4c3d4ad703750eff7c438b24e8
Marc117 <43848972+Marc117@users.noreply.github.com>
2021-06-24 17:00:08 +0200

    Shadow automation (#8)
    * added shadow automation plugin as a maven dependency
    
    (cherry picked from commit 9ee2bd59b2f0048d8fddb8f97bd1fd32ba51f706)
    
    * Method click shadow dom to allow automation with shadow dom elements
    
    (cherry picked from commit 9c439dac5cf633c0696d5033fe8ab279119b2aff)
    
    * Click method can handle shadow dom elements with syntax:
    tagname#id
    tagname[attributename="attributevalue"]
    outertagname<innertag....
    
    (cherry picked from commit c2355cb8f04d89e02b925a94dc34dae036c2884b)
    
    * Cleaned up code
    
    (cherry picked from commit 56b0f9195770648c3f2ec986d29f3428130d0dd8)
    
    * Delete ShadowDom_Own_Site.wiki
    
    (cherry picked from commit 8d5c47403c82d35bc8c724288fec53ff2a0ffd77)
    
    * getElement and getContainer now support Shadow Dom Elements
    
    (cherry picked from commit 45d9672cac7df517924d1a22e5990942d9a0303b)
    
    * getElement and getContainer now support Shadow Dom Elements
    
    (cherry picked from commit 2283eb288b3720f7a81ff87c455e9f7e5d656547)
    
    * Added Exception Handler to handle the ElementNotVisibleException caused by the Shadow Class, occures when a shadow dom element couldn't be found by the shadow class. Now it tries several times to find the element. The exception will still get printed to system out
    
    (cherry picked from commit 6e2343bc15861ee726338c1434b16b2f985ee131)
    
    * clean up
    
    (cherry picked from commit b8a351e99d55f705c3e1d015e7cab5d097ad5e04)
    
    * clean up
    
    (cherry picked from commit 32837216b5e125b5d31fafaa3458762d02397d56)
    
    * Shadom Dom Handling: - added methods to enable/disable usage of shadow automation class
    - added first example page
    
    (cherry picked from commit adaeaae104234fda32a52590cedc2fb159b0b65d)
    
    * cleanup removed .* imports
    
    (cherry picked from commit 525b3cbeef82208e2a8b68ca8459f2ec1486b3e3)
    
    * added description in BrowserTests.wiki for enabling/disabling shadow dom handling
    changes to BrowserTest
    
    (cherry picked from commit f910b6e20327ae745ef2e1bbe638c6cbbe88232b)
    
    * added new methods for setting implicit und explicit wait time of shadow class
    added info to BrowserTests.wiki and ShadowDomTest.wiki
    
    (cherry picked from commit a6d7f023e66b9533b259a9bb5dd474aee74b954a)
    
    * url encode method in BrowserTest added
    
    (cherry picked from commit c98c9a2dcef97ace51068a3dab8577739ee32fda)
    
    * Update BrowserTest.java
    
    (cherry picked from commit aca130d22e9ff2b455308d3a80da8608278b7f0b)
    
    * Update CustomFixtureTest.wiki
    
    (cherry picked from commit a3e4cb5e3e6eca9ac491eb3c1a4fefcc53de3bfb)
    
    * Update SeleniumHelper.java
    
    (cherry picked from commit 67bd416df2089b21de3ea6201076fd4fb4c6b66c)
    
    * ElementToCheckVisibility supports shadow DOM handling
    
    (cherry picked from commit 1debd4237fd89385df7e1c00ba382b939d3676fd)
    
    * cleaned up code
    
    (cherry picked from commit 2f8d1bd911a41a22dd37f6c9d86efb5c6a64d6d3)
    
    * Update BrowserTest.java
    
    (cherry picked from commit 078e21b898ef617062356874bc1c90d1adef0381)
    
    * Update FixtureDebugTest.java
    
    cleanup
    
    (cherry picked from commit b1e42dbca0e580c82dd05bdd2dc2245f6f6b75be)
    
    * Delete UrlEncodeTest.wiki
    
    (cherry picked from commit 0f99c5294c557180abc2371b3b3bd354fa859784)
    
    * cleanup
    
    (cherry picked from commit 3e571933e4d1448fb94ce7ea1221d7ae69c1cca3)
    
    * added example and acceptance test
    
    (cherry picked from commit eece2c4c7658986e1eb916505077bc07890470ea)
    
    * added example and acceptance test
    
    (cherry picked from commit 7052c0f8af84d48810f1790b08549cf3ac18a41a)
    
    * cleanup .gitignore
    
    (cherry picked from commit 4e7454b16d3b3230364340dc8c67614532862350)
    
    * shadow-automation: cleanup mistakes from cherry picking
    
    * shadow-automation: small adjustment to example

--- src/main/java/nl/hsac/fitnesse/fixture/slim/web/BrowserTest.java
+++ src/main/java/nl/hsac/fitnesse/fixture/slim/web/BrowserTest.java
@@ -28,6 +28,7 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.ElementNotVisibleException;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.TimeoutException;
@@ -38,6 +39,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import io.github.sukgu.Shadow;
 
 import java.io.File;
 import java.io.IOException;
@@ -74,6 +76,9 @@ public class BrowserTest<T extends WebElement> extends SlimFixture {
             EDGE_HIDDEN_BY_OTHER_ELEMENT_ERROR = "Element is obscured";
     private static final Pattern FIREFOX_HIDDEN_BY_OTHER_ELEMENT_ERROR_PATTERN =
             Pattern.compile("Element.+is not clickable.+because another element.+obscures it");
+    private Shadow shadow = new Shadow(driver());
+    private boolean shadowDomHandling = true;
+    private int shadowPollingTimeSeconds = 1;
 
     protected List<String> getCurrentSearchContextPath() {
         return currentSearchContextPath;
@@ -987,6 +992,9 @@ public class BrowserTest<T extends WebElement> extends SlimFixture {
     }
 
     protected T getElementToClick(String place) {
+        if (getSeleniumHelper().getElementToClick(place) == null && isShadowDomHandlingEnabled()){
+            return getElementWithShadow(place);
+        }
         return getSeleniumHelper().getElementToClick(place);
     }
 
@@ -1064,6 +1072,9 @@ public class BrowserTest<T extends WebElement> extends SlimFixture {
     }
 
     protected T getContainerElement(String container) {
+        if(findByTechnicalSelectorOr(container, this::getContainerImpl) == null && isShadowDomHandlingEnabled()){
+            return getElementWithShadow(container);
+        }
         return findByTechnicalSelectorOr(container, this::getContainerImpl);
     }
 
@@ -1593,6 +1604,9 @@ public class BrowserTest<T extends WebElement> extends SlimFixture {
     }
 
     protected T getElement(String place) {
+        if (getSeleniumHelper().getElement(place) == null && isShadowDomHandlingEnabled()){
+            return getElementWithShadow(place);
+        }
         return getSeleniumHelper().getElement(place);
     }
 
@@ -1929,6 +1943,9 @@ public class BrowserTest<T extends WebElement> extends SlimFixture {
     }
 
     protected T getElementToCheckVisibility(String place) {
+        if(getSeleniumHelper().getElementToCheckVisibility(place) == null && isShadowDomHandlingEnabled()){
+            return getElementWithShadow(place);
+        }
         return getSeleniumHelper().getElementToCheckVisibility(place);
     }
 
@@ -2730,6 +2747,38 @@ public class BrowserTest<T extends WebElement> extends SlimFixture {
 
     public void setNgBrowserTest(NgBrowserTest ngBrowserTest) {
         this.ngBrowserTest = ngBrowserTest;
+    }
+
+    public boolean isShadowDomHandlingEnabled(){
+        return shadowDomHandling;
+    }
+
+    public void setShadowDomHandlingTo(boolean shadowDomHandling){
+        this.shadowDomHandling = shadowDomHandling;
+    }
+
+    protected T getElementWithShadow(String cssSelector){
+        try{
+            return (T) shadow.findElement(cssSelector);
+        } catch (ElementNotVisibleException e){
+            return null;
+        }
+    }
+
+    public void setImplicitWaitForShadowTo(int shadowImplicitWaitSeconds){
+        shadow.setImplicitWait(shadowImplicitWaitSeconds);
+    }
+
+    public void setExplicitWaitForShadowTo(int shadowExplicitWaitSeconds){
+        try{
+            shadow.setExplicitWait(shadowExplicitWaitSeconds,shadowPollingTimeSeconds);
+        } catch(Exception e){
+            System.err.println(e);
+        }
+    }
+
+    public void setPollingTimeForShadowTo(int shadowPollingTimeSeconds){
+        this.shadowPollingTimeSeconds = shadowPollingTimeSeconds;
     }
 
     public boolean isImplicitWaitForAngularEnabled() {
