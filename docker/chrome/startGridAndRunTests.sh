4af878b86588a4fd2bb6bcc6b6f5cb6a035ad719
Fried Hoeben <github@hsac.nl>
2023-11-21 09:55:47 +0100

    environment variable to control whether ownership should be changed
    [ci skip]

--- docker/chrome/startGridAndRunTests.sh
+++ docker/chrome/startGridAndRunTests.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+if [ "${CHANGE_FITNESSE_DIRECTOY_OWNER}" = "true" ]; then
+  # ensure user of container is owner of all mounted volumes
+  owner="$(whoami)"
+  user_id="$(id -u "${owner}")"
+  group_id="$(id -g "${owner}")"
+  sudo chown -R "${user_id}:${group_id}" /fitnesse/
+fi
+
 /opt/bin/entry_point.sh &
 while [ ! -e /tmp/.X11-unix/X99 ]; do sleep 0.1; done
 
