db76f2c676c1b771d86c71cc39d3a030a514e09f
Roy de Kleijn <dekleijn.roy@gmail.com>
2024-01-08 08:59:45 +0100

    new examples based on v4 of practicesoftwaretesting.com (#593)
    [ci skip]

--- src/main/java/nl/hsac/fitnesse/sample/PracticeSoftwareTestingTest.java
+++ src/main/java/nl/hsac/fitnesse/sample/PracticeSoftwareTestingTest.java
@@ -1,25 +1,32 @@
 package nl.hsac.fitnesse.sample;
 
-import nl.hsac.fitnesse.fixture.slim.web.BrowserTest;
+import nl.hsac.fitnesse.fixture.slim.web.Ng2BrowserTest;
 import nl.hsac.fitnesse.fixture.slim.web.annotation.TimeoutPolicy;
 import nl.hsac.fitnesse.fixture.slim.web.annotation.WaitUntil;
 import org.openqa.selenium.WebElement;
 
 /**
- * Fixture class customized to test allmusic.com.
+ * Fixture class customized to test practicesoftwaretesting.com.
  */
-public class AllMusicTest extends BrowserTest {
+public class PracticeSoftwareTestingTest extends Ng2BrowserTest {
     // This method is very similar to what could be achieved using a scenario table
     @WaitUntil(TimeoutPolicy.RETURN_NULL)
-    public String birthData() {
-        return valueOf("xpath=//div[h4[contains(text(),'Born')]]/div");
+    public String city() {
+        return valueOf("xpath=//input[@data-test= 'city']");
     }
 
     // Custom fixture can customize heuristic, to first look at our custom location, before using the standard
     @Override
     protected WebElement getElementToRetrieveValue(String place, String container) {
         return findFirstInContainer(container, place,
-                () -> findByXPath("//div[h4[contains(text(),'%s')]]/div", place),
+                () -> findByXPath("//*[@data-test= '%s']", place),
+                () -> super.getElementToRetrieveValue(place, null));
+    }
+
+    @Override
+    protected WebElement getElementToSendValue(String place, String container) {
+        return findFirstInContainer(container, place,
+                () -> findByXPath("//*[@data-test= '%s']", place),
                 () -> super.getElementToRetrieveValue(place, null));
     }
 }
